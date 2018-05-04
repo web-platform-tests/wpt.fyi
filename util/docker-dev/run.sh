9ad33e56ad813afe7b53f9b74bb7c89189a637cc
Luke Bjerring <lukebjerring@google.com>
2018-05-04 12:01:16 -0400

    Re-use already running image

--- util/docker-dev/run.sh
+++ util/docker-dev/run.sh
@@ -25,25 +25,40 @@ WPTD_HOST_API_WEB_PORT=${WPTD_HOST_API_WEB_PORT:-"9999"}
 # wptd-dev                                  Identify image to use
 # /wpt.fyi/util/docker/inner/watch.sh       Identify code to execute
 
+info "Creating docker instance for dev server. Instance name: wptd-dev-instance"
+docker inspect wptd-dev-instance > /dev/null 2>&1
+INSPECT_STATUS="${?}"
+
 set -e
 
-info "Creating docker instance for dev server. Instance name: wptd-dev-instance"
-docker run -t -d --entrypoint /bin/bash \
-    -v "${WPTD_PATH}":/home/jenkins/wpt.fyi \
-    -u $(id -u $USER):$(id -g $USER) \
-    -p "${WPTD_HOST_WEB_PORT}:8080" \
-    -p "${WPTD_HOST_ADMIN_WEB_PORT}:8000" \
-    -p "${WPTD_HOST_API_WEB_PORT}:9999" \
-    --name wptd-dev-instance wptd-dev
+DOCKER_INSTANCE_PID=""
+if [ "${INSPECT_STATUS}" != "0" ]; then
+  info "Docker instance wptd-dev-instance not found. Starting it..."
+  docker run -t -d --entrypoint /bin/bash \
+      -v "${WPTD_PATH}":/home/jenkins/wpt.fyi \
+      -u $(id -u $USER):$(id -g $USER) \
+      -p "${WPTD_HOST_WEB_PORT}:8080" \
+      -p "${WPTD_HOST_ADMIN_WEB_PORT}:8000" \
+      -p "${WPTD_HOST_API_WEB_PORT}:9999" \
+      --name wptd-dev-instance wptd-dev
+  DOCKER_INSTANCE_PID="${!}"
+else
+  info "Found existing docker instance wptd-dev-instance"
+fi
 
+info "Ensuring current users has root..."
 wptd_chown "/home/jenkins"
 
 function stop() {
   warn "run.sh: Recieved interrupt. Exiting..."
+  info "Stopping wptd-dev-instance..."
   wptd_stop
+  info "Removing wptd-dev-instance..."
   wptd_rm
   exit 0
 }
+
+info "Instance wptd-dev-instance started."
 
 trap stop INT
 
