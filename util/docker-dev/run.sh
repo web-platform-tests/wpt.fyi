997a2b24b0d69db8b5acbebde53a29646c099cc8
Mark Dittmer <markdittmer@google.com>
2018-05-17 09:59:25 -0400

    run.sh: Carefully manage docker dev instance state

--- util/docker-dev/run.sh
+++ util/docker-dev/run.sh
@@ -13,6 +13,23 @@ WPTD_HOST_WEB_PORT=${WPTD_HOST_WEB_PORT:-"8080"}
 WPTD_HOST_ADMIN_WEB_PORT=${WPTD_HOST_ADMIN_WEB_PORT:-"8000"}
 WPTD_HOST_API_WEB_PORT=${WPTD_HOST_API_WEB_PORT:-"9999"}
 
+PR=""
+
+function usage() {
+  USAGE="USAGE: $(basename ${0}) [-q]
+    -q  quiet mode: Assume default for all prompts"
+  >&2 echo "${USAGE}"
+}
+
+QUIET="false"
+while getopts ':hq' FLAG; do
+  case "${FLAG}" in
+    q)
+      QUIET="true" ;;
+    h|*) usage && exit 0 ;;
+  esac
+done
+
 # Create a docker instance:
 #
 # --rm                                      Auto-remove when stopped
@@ -28,13 +45,22 @@ WPTD_HOST_API_WEB_PORT=${WPTD_HOST_API_WEB_PORT:-"9999"}
 info "Creating docker instance for dev server. Instance name: wptd-dev-instance"
 docker inspect wptd-dev-instance > /dev/null 2>&1
 INSPECT_STATUS="${?}"
+docker inspect wptd-dev-instance | grep '"Running": true' | read
+RUNNING_STATUS="${?}"
 
 function stop() {
   warn "run.sh: Recieved interrupt. Exiting..."
   info "Stopping wptd-dev-instance..."
   wptd_stop
-  info "Removing wptd-dev-instance..."
-  wptd_rm
+  info "wptd-dev-instance stopped."
+  if [[ "${QUIET}" != "true" ]]; then
+    read -p "Docker instance wpt-dev-instance: (p)reserve or (r)emove (P/r): " PR
+  fi
+  if [[ "${PR}" == "r" ]] || [[ "${PR}" == "R" ]]; then
+    info "Removing wptd-dev-instance..."
+    wptd_rm
+    info "wptd-dev-instance removed."
+  fi
 }
 
 function quit() {
@@ -42,31 +68,46 @@ function quit() {
   exit 0
 }
 
-DOCKER_INSTANCE_PID=""
 if [ "${INSPECT_STATUS}" == "0" ]; then
-  info "Found existing docker instance wptd-dev-instance. Killing it..."
-  stop
+  info "Found existing docker instance wptd-dev-instance."
+  if [[ "${QUIET}" != "true" ]]; then
+    read -p "Docker instance wpt-dev-instance: (p)reserve or (r)emove (P/r): " PR
+  fi
+  if [[ "${PR}" == "r" ]] || [[ "${PR}" == "R" ]]; then
+    stop
+  fi
 fi
 
 set -e
 
-info "Starting docker instance wptd-dev-instance..."
-docker run -t -d --entrypoint /bin/bash \
-    -v "${WPTD_PATH}":/home/user/wpt.fyi \
-    -u $(id -u $USER):$(id -g $USER) \
-    -p "${WPTD_HOST_WEB_PORT}:8080" \
-    -p "${WPTD_HOST_ADMIN_WEB_PORT}:8000" \
-    -p "${WPTD_HOST_API_WEB_PORT}:9999" \
-    --name wptd-dev-instance wptd-dev
-DOCKER_INSTANCE_PID="${!}"
 
-info "Setting up local user"
-wptd_useradd
 
-info "Ensuring the home directory is owned by the user..."
-wptd_chown "/home/user"
+if [[ "${INSPECT_STATUS}" != 0 ]] || [[ "${PR}" == "r" ]] || [[ "${PR}" == "R" ]]; then
+  info "Starting docker instance wptd-dev-instance..."
+  docker run -t -d --entrypoint /bin/bash \
+      -v "${WPTD_PATH}":/home/user/wpt.fyi \
+      -u $(id -u $USER):$(id -g $USER) \
+      -p "${WPTD_HOST_WEB_PORT}:8080" \
+      -p "${WPTD_HOST_ADMIN_WEB_PORT}:8000" \
+      -p "${WPTD_HOST_API_WEB_PORT}:9999" \
+      --name wptd-dev-instance wptd-dev
+  info "Setting up local user"
+  wptd_useradd
 
-info "Instance wptd-dev-instance started."
+  info "Ensuring the home directory is owned by the user..."
+  wptd_chown "/home/user"
+
+  info "Instance wptd-dev-instance started."
+elif [[ "${RUNNING_STATUS}" != "0" ]]; then
+  info "Restarting docker instance wptd-dev-instance..."
+  docker start wptd-dev-instance
+  info "Instance wptd-dev-instance restarted."
+else
+  info "Docker instance wptd-dev-instance already running."
+  exit 0
+fi
+
+
 
 trap quit INT
 
