3fefdd2b95c7b485231b35aa6b6d80b8f207ce69
Mark Dittmer <markdittmer@google.com>
2018-05-18 15:41:30 -0400

    File header nonsense

--- revisions/epoch/gregorian_test.go
+++ revisions/epoch/gregorian_test.go
@@ -1,5 +1,9 @@
 // +build small
 
+// Copyright 2018 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package epoch_test
 
 import (
