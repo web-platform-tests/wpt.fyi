acba634e225e462690c1ad6c6ea57f0faf448474
Luke Bjerring <lukebjerring@google.com>
2018-05-30 11:58:17 -0400

    Add staging.wpt.fyi continuous webdriver test

--- webdriver/search_test.go
+++ webdriver/search_test.go
@@ -3,17 +3,11 @@
 package webdriver
 
 import (
-	"context"
-	"fmt"
 	"testing"
 	"time"
 
-	"log"
-
 	"github.com/stretchr/testify/assert"
 	"github.com/tebeka/selenium"
-	"github.com/web-platform-tests/wpt.fyi/shared"
-	"google.golang.org/appengine/datastore"
 )
 
 func TestSearch(t *testing.T) {
@@ -22,20 +16,13 @@ func TestSearch(t *testing.T) {
 		panic(err)
 	}
 	defer devAppserverInstance.Close()
-	if err = devAppserverInstance.AwaitReady(); err != nil {
-		panic(err)
-	}
-
-	if err = addStaticData(devAppserverInstance); err != nil {
-		panic(err)
-	}
 
 	service, wd, err := FirefoxWebDriver()
 	defer service.Stop()
 	defer wd.Quit()
 
 	// Navigate to the wpt.fyi homepage.
-	if err := wd.Get(devAppserverInstance.GetWebappUrl("/")); err != nil {
+	if err := wd.Get(devAppserverInstance.GetWebappURL("/")); err != nil {
 		panic(err)
 	}
 
@@ -70,80 +57,4 @@ func TestSearch(t *testing.T) {
 		assert.Fail(t, err.Error())
 	}
 	assert.Equal(t, "2dcontext/", text)
-}
-
-func addStaticData(i WebserverInstance) (err error) {
-	var ctx context.Context
-	if ctx, err = i.NewContext(); err != nil {
-		return err
-	}
-
-	staticDataTime, _ := time.Parse(time.RFC3339, "2017-10-18T00:00:00Z")
-	// Follow pattern established in run/*.py data collection code.
-	const sha = "b952881825"
-	const summaryURLFmtString = "/static/" + sha + "/%s"
-	staticTestRuns := []shared.TestRun{
-		{
-			ProductAtRevision: shared.ProductAtRevision{
-				Product: shared.Product{
-					BrowserName:    "chrome",
-					BrowserVersion: "63.0",
-					OSName:         "linux",
-					OSVersion:      "3.16",
-				},
-				Revision: sha,
-			},
-			ResultsURL: fmt.Sprintf(summaryURLFmtString, "chrome-63.0-linux-summary.json.gz"),
-			CreatedAt:  staticDataTime,
-		},
-		{
-			ProductAtRevision: shared.ProductAtRevision{
-				Product: shared.Product{
-					BrowserName:    "edge",
-					BrowserVersion: "15",
-					OSName:         "windows",
-					OSVersion:      "10",
-				},
-				Revision: sha,
-			},
-			ResultsURL: fmt.Sprintf(summaryURLFmtString, "edge-15-windows-10-sauce-summary.json.gz"),
-			CreatedAt:  staticDataTime,
-		},
-		{
-			ProductAtRevision: shared.ProductAtRevision{
-				Product: shared.Product{
-					BrowserName:    "firefox",
-					BrowserVersion: "57.0",
-					OSName:         "linux",
-					OSVersion:      "*",
-				},
-				Revision: sha,
-			},
-			ResultsURL: fmt.Sprintf(summaryURLFmtString, "firefox-57.0-linux-summary.json.gz"),
-			CreatedAt:  staticDataTime,
-		},
-		{
-			ProductAtRevision: shared.ProductAtRevision{
-				Product: shared.Product{
-					BrowserName:    "safari",
-					BrowserVersion: "10",
-					OSName:         "macos",
-					OSVersion:      "10.12",
-				},
-				Revision: sha,
-			},
-			ResultsURL: fmt.Sprintf(summaryURLFmtString, "safari-10.0-macos-10.12-sauce-summary.json.gz"),
-			CreatedAt:  staticDataTime,
-		},
-	}
-
-	log.Println("Adding static TestRun data...")
-	for i := range staticTestRuns {
-		key := datastore.NewIncompleteKey(ctx, "TestRun", nil)
-		if _, err := datastore.Put(ctx, key, &staticTestRuns[i]); err != nil {
-			return err
-		}
-		fmt.Printf("Added static run for %s\n", staticTestRuns[i].BrowserName)
-	}
-	return nil
 }
