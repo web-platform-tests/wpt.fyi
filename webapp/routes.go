bc4ebc5edd9cef78e26acc1f30d877541fbb5ec9
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-06-08 13:18:55 -0400

    Fix results paths with slashes (#253)
    Default regex is [^/]*, which doesn't work for paths containing a slash.

--- webapp/routes.go
+++ webapp/routes.go
@@ -29,7 +29,7 @@ func RegisterRoutes() {
 
 	// Test run results, viewed by pass-rate across the browsers
 	shared.AddRoute("/interop/", "interop", interopHandler)
-	shared.AddRoute("/interop/{path}", "interop", interopHandler)
+	shared.AddRoute("/interop/{path:.*}", "interop", interopHandler)
 
 	// List of all test runs, by SHA[0:10]
 	shared.AddRoute("/test-runs", "test-runs", testRunsHandler)
@@ -40,9 +40,9 @@ func RegisterRoutes() {
 	// Test run results, viewed by browser (default view)
 	// For run results diff view, 'before' and 'after' params can be given.
 	shared.AddRoute("/results/", "results", testResultsHandler)
-	shared.AddRoute("/results/{path}", "results", testResultsHandler)
+	shared.AddRoute("/results/{path:.*}", "results", testResultsHandler)
 
 	// Legacy wildcard match
 	shared.AddRoute("/", "results-legacy", testResultsHandler)
-	shared.AddRoute("/{path}", "results-legacy", testResultsHandler)
+	shared.AddRoute("/{path:.*}", "results-legacy", testResultsHandler)
 }
