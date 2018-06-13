3873b35220493dbb1eeddb815a38c5ab0bb94ee4
Mark Dittmer <mark.s.dittmer@gmail.com>
2018-06-12 22:03:34 -0400

    Add tests for epoch timezone management

--- revisions/epoch/fractional_test.go
+++ revisions/epoch/fractional_test.go
@@ -33,8 +33,17 @@ func testFarPositive(t *testing.T, e epoch.Epoch) {
 	assert.True(t, e.IsEpochal(thisYear, lastYear))
 }
 
+func testTZPositive(t *testing.T, e epoch.Epoch) {
+	n := int(e.GetData().MaxDuration.Hours())
+	justPrior := time.Date(2018, 4, 1, n-1, 59, 59, 999999999, time.UTC)
+	justAfter := time.Date(2018, 4, 1, n-1, 0, 0, 0, time.FixedZone("UTC-1", -60*60))
+	assert.True(t, e.IsEpochal(justPrior, justAfter))
+	assert.True(t, e.IsEpochal(justAfter, justPrior))
+}
+
 var startDay = time.Date(2018, 4, 1, 0, 0, 0, 0, time.UTC)
 var justAfterStartDay = time.Date(2018, 4, 1, 0, 0, 0, 1, time.UTC)
+var justAfterStartDayTZ = time.Date(2018, 3, 31, 23, 0, 0, 1, time.FixedZone("UTC-1", -60*60))
 
 func testCloseNegative(t *testing.T, e epoch.Epoch) {
 	assert.False(t, e.IsEpochal(startDay, justAfterStartDay))
@@ -49,6 +58,11 @@ func testFarNegative(t *testing.T, e epoch.Epoch) {
 	assert.False(t, e.IsEpochal(justBeforeEnd, start))
 }
 
+func testTZNegative(t *testing.T, e epoch.Epoch) {
+	assert.False(t, e.IsEpochal(startDay, justAfterStartDayTZ))
+	assert.False(t, e.IsEpochal(justAfterStartDayTZ, startDay))
+}
+
 //
 // EightHourly
 //
@@ -61,10 +75,74 @@ func TestIsEightHourly_Far(t *testing.T) {
 	testFarPositive(t, eightHourly)
 }
 
+func TestIsEightHourly_TZ(t *testing.T) {
+	testTZPositive(t, eightHourly)
+}
+
 func TestIsNotEightHourly_Close(t *testing.T) {
 	testCloseNegative(t, eightHourly)
 }
 
 func TestIsNotEightHourly_Far(t *testing.T) {
 	testFarNegative(t, eightHourly)
+}
+
+func TestIsNotEightHourly_TZ(t *testing.T) {
+	testTZNegative(t, eightHourly)
+}
+
+//
+// FourHourly
+//
+
+func TestIsFourHourly_Close(t *testing.T) {
+	testClosePositive(t, fourHourly)
+}
+
+func TestIsFourHourly_Far(t *testing.T) {
+	testFarPositive(t, fourHourly)
+}
+
+func TestIsFourHourly_TZ(t *testing.T) {
+	testTZPositive(t, fourHourly)
+}
+
+func TestIsNotFourHourly_Close(t *testing.T) {
+	testCloseNegative(t, fourHourly)
+}
+
+func TestIsNotFourHourly_Far(t *testing.T) {
+	testFarNegative(t, fourHourly)
+}
+
+func TestIsNotFourHourly_TZ(t *testing.T) {
+	testTZNegative(t, fourHourly)
+}
+
+//
+// TwoHourly
+//
+
+func TestIsTwoHourly_Close(t *testing.T) {
+	testClosePositive(t, twoHourly)
+}
+
+func TestIsTwoHourly_Far(t *testing.T) {
+	testFarPositive(t, twoHourly)
+}
+
+func TestIsTwoHourly_TZ(t *testing.T) {
+	testTZPositive(t, twoHourly)
+}
+
+func TestIsNotTwoHourly_Close(t *testing.T) {
+	testCloseNegative(t, twoHourly)
+}
+
+func TestIsNotTwoHourly_Far(t *testing.T) {
+	testFarNegative(t, twoHourly)
+}
+
+func TestIsNotTwoHourly_TZ(t *testing.T) {
+	testTZNegative(t, twoHourly)
 }
