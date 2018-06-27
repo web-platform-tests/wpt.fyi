ad208838640d1c11981186edab5fef607352c7ca
Robert Ma <robertma@chromium.org>
2018-06-27 18:37:08 -0400

    Improve tests

--- shared/browsers_test.go
+++ shared/browsers_test.go
@@ -16,6 +16,10 @@ import (
 func TestGetDefaultBrowserNames(t *testing.T) {
 	names := GetDefaultBrowserNames()
 	assert.True(t, sort.StringsAreSorted(names))
+	// Non default browser names:
+	for _, n := range names {
+		assert.NotEqual(t, "uc", n)
+	}
 }
 
 func TestIsBrowserName(t *testing.T) {
@@ -23,10 +27,11 @@ func TestIsBrowserName(t *testing.T) {
 	assert.True(t, IsBrowserName("edge"))
 	assert.True(t, IsBrowserName("firefox"))
 	assert.True(t, IsBrowserName("safari"))
+	assert.True(t, IsBrowserName("uc"))
 	assert.False(t, IsBrowserName("not-a-browser"))
 }
 
-func TestIsBrowserName_Names(t *testing.T) {
+func TestIsBrowserName_DefaultBrowsers(t *testing.T) {
 	names := GetDefaultBrowserNames()
 	for _, name := range names {
 		assert.True(t, IsBrowserName(name))
