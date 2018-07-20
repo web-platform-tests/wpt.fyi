d865ed51db061cde8a3b2bb5d2dcf89de5732cb4
Robert Ma <bob1211@gmail.com>
2018-07-20 17:05:55 -0400

    Remove some out-of-date warnings about creds (#370)
    I forgot to remove these in #348.

--- util/docker-dev/dev_data.sh
+++ util/docker-dev/dev_data.sh
@@ -2,29 +2,7 @@
 
 DOCKER_DIR=$(dirname $0)
 source "${DOCKER_DIR}/../commands.sh"
-source "${DOCKER_DIR}/../logging.sh"
 source "${DOCKER_DIR}/../path.sh"
-WPTD_PATH=${WPTD_PATH:-$(absdir ${DOCKER_DIR}/../..)}
 
 # Run util/populate_dev_data.go (via make) in the docker environment.
-
-# This script copies the file found under the $GOOGLE_APPLICATION_CREDENTIALS
-# environment variable across to the docker instance, and sets the instance's
-# same environment variable to point to the copy.
-
-DOCKER_INSTANCE="${DOCKER_INSTANCE:-wptd-dev-instance}"
-DEFAULT_CREDS_FILE=/home/application_default_credentials.json
-COPY_COMMAND="docker cp ${GOOGLE_APPLICATION_CREDENTIALS} ${DOCKER_INSTANCE}:${DEFAULT_CREDS_FILE}"
-ENVIRONMENT_VAR="-e GOOGLE_APPLICATION_CREDENTIALS=${DEFAULT_CREDS_FILE}"
-
-if [[ "${GOOGLE_APPLICATION_CREDENTIALS}" == "" ]]
-then
-    warn "Environment variable \$GOOGLE_APPLICATION_CREDENTIALS not set."
-    warn "See https://developers.google.com/accounts/docs/application-default-credentials for more information."
-    ENVIRONMENT_VAR=""
-else
-    info "${COPY_COMMAND}"
-    ${COPY_COMMAND}
-fi
-
 wptd_exec make dev_data FLAGS=\"$@\"
