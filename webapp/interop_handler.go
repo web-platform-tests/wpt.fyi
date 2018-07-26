112603b2e96947e207c825d80f163a1e18eedf6d
Luke Bjerring <lukebjerring@chromium.org>
2018-07-26 09:36:31 -0400

    Propagate query params to interop

--- webapp/interop_handler.go
+++ webapp/interop_handler.go
@@ -5,9 +5,11 @@
 package webapp
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/url"
 
+	"github.com/web-platform-tests/results-analysis/metrics"
 	"github.com/web-platform-tests/wpt.fyi/shared"
 )
 
@@ -22,13 +24,55 @@ func interopHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	sourceURL.RawQuery = f.ToQuery(true).Encode()
 
+	// We 'load by SHA' by fetching any interop result with all TestRunIDs for that SHA.
+	if !filters.IsDefaultQuery() {
+		// Load default browser runs for SHA.
+		// Ignore any max-count; makes no sense for a interop run.
+		one := 1
+		runs, err := shared.LoadTestRuns(
+			ctx, filters.GetProductsOrDefault(), filters.Labels, []string{filters.SHA}, filters.From, filters.To, &one)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		for _, run := range runs {
+			query = query.Filter("TestRunIDs =", run.ID)
+		}
+	}
+
+	var metadataSlice []metrics.PassRateMetadataLegacy
+	if _, err := query.GetAll(ctx, &metadataSlice); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if len(metadataSlice) != 1 {
+		http.Error(w, "No metrics runs found", http.StatusNotFound)
+		return
+	}
+
+	metadata := &metadataSlice[0]
+	if err := metadata.LoadTestRuns(ctx); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	uiFilters, err := parseTestRunUIFilter(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	metadataBytes, err := json.Marshal(*metadata)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 	data := struct {
-		Metadata        string
-		MetadataSources string
-		SHA             string
+		Metadata string
+		Filter   testRunUIFilter
 	}{
-		MetadataSources: sourceURL.String(),
-		SHA:             f.SHA,
+		Metadata: string(metadataBytes),
+		Filter:   uiFilters,
 	}
 
 	if err := templates.ExecuteTemplate(w, "interoperability.html", data); err != nil {
