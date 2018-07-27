3a5ccb7012aa01e26f250e541de07d0d51891da4
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-07-27 16:29:30 -0400

    Add integration test for test-file-results (#391)

---
+++ webdriver/file_results_test.go
@@ -0,0 +1,51 @@
+// +build large
+
+package webdriver
+
+import (
+	"testing"
+	"time"
+
+	"github.com/tebeka/selenium"
+)
+
+func TestFileResults(t *testing.T) {
+	app, err := NewWebserver()
+	if err != nil {
+		panic(err)
+	}
+	defer app.Close()
+
+	service, wd, err := GetWebDriver()
+	defer service.Stop()
+	defer wd.Quit()
+
+	// Navigate to an individual test file result.
+	url := "results/infrastructure/assumptions/ahem.html"
+	if err := wd.Get(app.GetWebappURL(url)); err != nil {
+		panic(err)
+	}
+
+	// Wait for the results view to load.
+	runsLoadedCondition := func(wd selenium.WebDriver) (bool, error) {
+		testRuns, err := getFileResultRows(wd)
+		if err != nil {
+			return false, err
+		}
+		return len(testRuns) > 0, nil
+	}
+	wd.WaitWithTimeout(runsLoadedCondition, time.Second*10)
+}
+
+func getFileResultRows(wd selenium.WebDriver) ([]selenium.WebElement, error) {
+	switch *browser {
+	case "firefox":
+		return wd.FindElements(selenium.ByCSSSelector, "tbody tr")
+	default:
+		e, err := wd.FindElement(selenium.ByCSSSelector, "wpt-results")
+		if err != nil {
+			return nil, err
+		}
+		return FindShadowElements(wd, e, "test-file-results", "tbody tr")
+	}
+}
