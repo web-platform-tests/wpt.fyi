8bdff5e467379e4fd03dd40c42d36471528b88b8
Mark Dittmer <markdittmer@google.com>
2018-09-04 14:00:20 -0400

    Abstract shared.Logger interface compatible with GAE logging

--- shared/util.go
+++ shared/util.go
@@ -5,7 +5,14 @@
 package shared
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
 	mapset "github.com/deckarep/golang-set"
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	gaelog "google.golang.org/appengine/log"
 )
 
 // ExperimentalLabel is the implicit label present for runs marked 'experimental'.
@@ -39,4 +46,117 @@ func ToStringSlice(set mapset.Set) []string {
 // of which are treated as looking up the latest run for each browser.
 func IsLatest(sha string) bool {
 	return sha == "" || sha == "latest"
+}
+
+type Logger interface {
+	Criticalf(format string, args ...interface{})
+	Debugf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Warningf(format string, args ...interface{})
+}
+
+type gaeLogger struct {
+	ctx context.Context
+}
+
+type stdLogger struct{}
+
+type nilLogger struct{}
+
+func (l gaeLogger) Criticalf(format string, args ...interface{}) {
+	gaelog.Criticalf(l.ctx, format, args...)
+}
+
+func (l gaeLogger) Debugf(format string, args ...interface{}) {
+	gaelog.Criticalf(l.ctx, format, args...)
+}
+
+func (l gaeLogger) Errorf(format string, args ...interface{}) {
+	gaelog.Errorf(l.ctx, format, args...)
+}
+
+func (l gaeLogger) Infof(format string, args ...interface{}) {
+	gaelog.Infof(l.ctx, format, args...)
+}
+
+func (l gaeLogger) Warningf(format string, args ...interface{}) {
+	gaelog.Warningf(l.ctx, format, args...)
+}
+
+func (l stdLogger) Criticalf(format string, args ...interface{}) {
+	log.Printf("CRIT: %s", fmt.Sprintf(format, args...))
+}
+
+func (l stdLogger) Debugf(format string, args ...interface{}) {
+	log.Printf("DEBG: %s", fmt.Sprintf(format, args...))
+}
+
+func (l stdLogger) Errorf(format string, args ...interface{}) {
+	log.Printf("ERRO: %s", fmt.Sprintf(format, args...))
+}
+
+func (l stdLogger) Infof(format string, args ...interface{}) {
+	log.Printf("INFO: %s", fmt.Sprintf(format, args...))
+}
+
+func (l stdLogger) Warningf(format string, args ...interface{}) {
+	log.Printf("WARN: %s", fmt.Sprintf(format, args...))
+}
+
+func (l nilLogger) Criticalf(format string, args ...interface{}) {}
+
+func (l nilLogger) Debugf(format string, args ...interface{}) {}
+
+func (l nilLogger) Errorf(format string, args ...interface{}) {}
+
+func (l nilLogger) Infof(format string, args ...interface{}) {}
+
+func (l nilLogger) Warningf(format string, args ...interface{}) {}
+
+// LoggerCtxKey is a key for attaching a Logger to a context.Context.
+type LoggerCtxKey struct{}
+
+var (
+	gl  = gaeLogger{}
+	sl  = stdLogger{}
+	nl  = nilLogger{}
+	lck = LoggerCtxKey{}
+)
+
+// NewGAELogger returns a Google App Engine Standard Environment logger bound to
+// the given context.
+func NewGAELogger(ctx context.Context) Logger {
+	return gaeLogger{ctx}
+}
+
+// NewSTDLogger returns a new standard logger.
+func NewSTDLogger() Logger {
+	return sl
+}
+
+// NewNilLogger returns a new logger that silently ignores all Logger calls.
+func NewNilLogger() Logger {
+	return nl
+}
+
+// DefaultLoggerCtxKey returns the default key where a logger instance should be
+// stored in a context.Context object.
+func DefaultLoggerCtxKey() LoggerCtxKey {
+	return lck
+}
+
+// NewAppEngineContext creates a new Google App Engine-based context bound to
+// an http.Request.
+func NewAppEngineContext(r *http.Request) context.Context {
+	ctx := appengine.NewContext(r)
+	ctx = context.WithValue(ctx, DefaultLoggerCtxKey(), NewGAELogger(ctx))
+	return ctx
+}
+
+// NewTestContext creates a new context.Context for small tests.
+func NewTestContext() context.Context {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, DefaultLoggerCtxKey(), NewNilLogger())
+	return ctx
 }
