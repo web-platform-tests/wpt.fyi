96740fbd3c467960173dea6c27d275c6df47d2fb
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-09-13 21:25:05 -0400

    Change test-runs to default to 1 month (#549)

--- webapp/test_runs_handler.go
+++ webapp/test_runs_handler.go
@@ -25,10 +25,10 @@ func testRunsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Get runs from 3 months ago onward by default.
+	// Get runs from a month ago, onward, by default.
 	if filter.From == nil {
-		threeMonthsAgo := time.Now().Truncate(time.Hour*24).AddDate(0, -3, 0)
-		filter.From = &threeMonthsAgo
+		aMonthAgo := time.Now().Truncate(time.Hour*24).AddDate(0, -1, 0)
+		filter.From = &aMonthAgo
 	}
 
 	query := filter.ToQuery()
