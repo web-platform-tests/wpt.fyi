cc6270bb7f354e036069afc14543528d48f23577
Luke Bjerring <lukebjerring@google.com>
2018-09-18 09:48:02 -0400

    Check aligned in TestLabelParam webdriver test

--- webdriver/label_test.go
+++ webdriver/label_test.go
@@ -7,8 +7,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/deckarep/golang-set"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/tebeka/selenium"
+	"github.com/web-platform-tests/wpt.fyi/shared"
 )
 
 func TestLabelParam(t *testing.T) {
@@ -25,14 +28,17 @@ func TestLabelParam(t *testing.T) {
 	defer service.Stop()
 	defer wd.Quit()
 
+	// Local static data only have 2 experimental browsers, and neither has aligned
+	// experimental runs.
 	if *staging {
-		// We have all 4 experimental browsers on staging.wpt.fyi.
-		testLabel(t, wd, app, "/", "experimental", "wpt-results", 4)
+		testLabel(t, wd, app, "/", "experimental", "wpt-results", 4, false)
 	} else {
-		// Local static data only have 2 experimental browsers.
-		testLabel(t, wd, app, "/", "experimental", "wpt-results", 2)
+		testLabel(t, wd, app, "/", "experimental", "wpt-results", 2, false)
 	}
-	testLabel(t, wd, app, "/interop", "stable", "wpt-interop", 4)
+
+	for _, aligned := range []bool{true, false} {
+		testLabel(t, wd, app, "/interop", "stable", "wpt-interop", 4, aligned)
+	}
 }
 
 func testLabel(
@@ -40,9 +46,14 @@ func testLabel(
 	wd selenium.WebDriver,
 	app AppServer,
 	path, label, elementName string,
-	runs int) {
+	runs int,
+	aligned bool) {
 	// Navigate to the wpt.fyi homepage.
-	url := fmt.Sprintf("%s?label=%s", path, label)
+	filters := shared.TestRunFilter{
+		Labels:  mapset.NewSetWith(label),
+		Aligned: &aligned,
+	}
+	url := fmt.Sprintf("%s?%s", path, filters.ToQuery().Encode())
 	if err := wd.Get(app.GetWebappURL(url)); err != nil {
 		panic(err)
 	}
@@ -63,6 +74,9 @@ func testLabel(
 		panic(err)
 	}
 	assert.Lenf(t, testRuns, runs, "Expected exactly %v TestRuns search result.", runs)
+	if aligned {
+		assertAligned(t, wd, testRuns)
+	}
 
 	// Check tab URLs propagate label
 	tabs, err := getTabElements(wd, elementName)
@@ -100,5 +114,20 @@ func getTabElements(wd selenium.WebDriver, element string) ([]selenium.WebElemen
 			return nil, err
 		}
 		return FindShadowElements(wd, e, "results-navigation", "paper-tab")
+	}
+}
+
+func assertAligned(t *testing.T, wd selenium.WebDriver, testRuns []selenium.WebElement) {
+	if len(testRuns) < 2 {
+		return
+	}
+	args := []interface{}{testRuns[0]}
+	shaProp := "return arguments[0].testRun.revision"
+	sha, _ := wd.ExecuteScriptRaw(shaProp, args)
+	assert.NotEqual(t, sha, "")
+	for i := 1; i < len(testRuns); i++ {
+		args = []interface{}{testRuns[0]}
+		otherSHA, _ := wd.ExecuteScriptRaw(shaProp, args)
+		assert.Equal(t, sha, otherSHA)
 	}
 }
