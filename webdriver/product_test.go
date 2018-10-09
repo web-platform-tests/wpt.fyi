web-platform-tests/wpt.fyi

commit 2ed638d00dde49251df9393c12e3107d659fc159

Show diff passes/regression numbers separately (#609)

webapp/components/wpt-results.html
@@ -90,10 +90,10 @@ found in the LICENSE file.
       .passes-80 { background-color: hsl(0, 85%, 85%); }
       .passes-100 { background-color: var(--paper-light-green-a400); }
 
-      span.delta.negative {
+      span.delta.regressions {
         color: var(--paper-red-700);
       }
-      span.delta.positive {
+      span.delta.passes {
         color: var(--paper-green-700);
       }
       td.none {
@@ -213,7 +213,12 @@ found in the LICENSE file.
                 <th><test-run test-run="[[testRun]]"></test-run></th>
               </template>
               <template is="dom-if" if="[[diffShown]]">
-                <th><test-run test-run="[[diffRun]]"></test-run></th>
+                <th>
+                  <test-run test-run="[[diffRun]]"></test-run>
+                  <template is="dom-if" if="[[diffFilter]]">
+                    <paper-icon-button icon="filter-list" onclick="[[toggleDiffFilter]]"></paper-icon-button>
+                  </template>
+                </th>
               </template>
               <template is="dom-if" if="[[queryBuilder]]">
                 <th>
@@ -240,9 +245,11 @@ found in the LICENSE file.
                 </template>
                 <template is="dom-if" if="[[diffShown]]">
                   <td class$="numbers [[ testResultClass(node, index, diffRun, 'passes') ]]">
-                    <span class$="passes [[ testResultClass(node, index, diffRun, 'passes') ]]">{{ getNodeResultDataByPropertyName(node, -1, diffRun, 'passes') }}</span>
+                    <span class="delta passes">{{ getNodeResultDataByPropertyName(node, -1, diffRun, 'passes') }}</span>
                     /
-                    <span class$="total [[ testResultClass(node, index, diffRun, 'total') ]]">{{ getNodeResultDataByPropertyName(node, -1, diffRun, 'total') }}</span>
+                    <span class="delta regressions">{{ getNodeResultDataByPropertyName(node, -1, diffRun, 'regressions') }}</span>
+                    /
+                    <span class="delta total">{{ getNodeResultDataByPropertyName(node, -1, diffRun, 'total') }}</span>
                   </td>
                 </template>
               </tr>
@@ -337,9 +344,13 @@ found in the LICENSE file.
             type: Boolean,
             computed: 'isDiffShown(diff, diffRun)',
           },
+          diffURL: {
+            type: String,
+            computed: 'computeDiffURL(testRuns, testRunQuery)',
+          },
           filter: {
             type: String,
-            value: 'ADCU', // Added, Deleted, Changed, Unchanged
+            value: 'ADC', // Added, Deleted, Changed
           },
           showHistory: {
             type: Boolean,
@@ -356,6 +367,7 @@ found in the LICENSE file.
             type: Boolean,
             value: false,
           },
+          onlyShowDifferences: Boolean,
         };
       }
 
@@ -425,6 +437,19 @@ found in the LICENSE file.
         return url;
       }
 
+      computeDiffURL(productSpecs, testRunQuery) {
+        if (!productSpecs || productSpecs.length !== 2) {
+          return;
+        }
+        let url = new URL('/api/diff', window.location);
+        url.search = testRunQuery
+          .replace('product', 'before')
+          .replace('product', 'after');
+        url.searchParams.delete('diff');
+        url.searchParams.set('filter', this.filter);
+        return url;
+      }
+
       constructor() {
         super();
         this.onSearchCommit = this.handleSearchCommit.bind(this);
@@ -435,6 +460,10 @@ found in the LICENSE file.
         };
         this.toggleQueryEdit = () => {
           this.editingQuery = !this.editingQuery;
+        };
+        this.toggleDiffFilter = () => {
+          this.onlyShowDifferences = !this.onlyShowDifferences;
+          this.refreshDisplayedNodes();
         };
         this.updateQuery = this.handleUpdateQuery.bind(this);
       }
@@ -471,7 +500,7 @@ found in the LICENSE file.
         this.loadData();
       }
 
-      async loadData() {
+      loadData() {
         this.load(
           this.loadRuns().then(async runs => {
             this.fetchResults();
@@ -482,26 +511,45 @@ found in the LICENSE file.
                 revision: 'diff',
                 browser_name: 'diff',
               };
+              this.fetchDiff();
             }
           })
         );
       }
 
-      async fetchResults() {
-        this.loadingCount++;
-        try {
-          const json = await window.fetch(this.searchURL)
+      fetchResults() {
+        this.load(
+          window.fetch(this.searchURL)
             .then(r => {
               if (!r.ok || r.status !== 200) {
                 return Promise.reject('Failed to fetch results data.');
               }
               return r.json();
-            });
-          this.searchResults = json.results;
-          this.refreshDisplayedNodes();
-        } finally {
-          this.loadingCount--;
+            })
+            .then(json => {
+              this.searchResults = json.results;
+              this.refreshDisplayedNodes();
+            })
+        );
+      }
+
+      fetchDiff() {
+        if (!this.diffFromAPI) {
+          return;
         }
+        this.load(
+          window.fetch(this.diffURL)
+            .then(r => {
+              if (!r.ok || r.status !== 200) {
+                return Promise.reject('Failed to fetch diff data.');
+              }
+              return r.json();
+            })
+            .then(json => {
+              this.diffResults = json;
+              this.refreshDisplayedNodes();
+            })
+        );
       }
 
       pathUpdated(path) {
@@ -552,26 +600,46 @@ found in the LICENSE file.
               // existing node.
               if (!nodes.hasOwnProperty(name)) {
                 nodes[name] = acc.length;
-                acc.push({
+                const row = {
                   path: `${prefix}${name}`,
                   isDir,
-                  // Copy legacy_status node contents.
-                  results: r.legacy_status
-                    .map(stats => Object.assign({}, stats)),
-                });
-              } else {
-                const rs = r.legacy_status;
-                const nrs = acc[nodes[name]].results;
-
-                for (let i = 0; i < rs.length; i++) {
-                  nrs[i].passes += rs[i].passes;
-                  nrs[i].total += rs[i].total;
-                }
+                  results: r.legacy_status.map(() => ({
+                    passes: 0,
+                    total: 0,
+                  })),
+                };
+                row.diff = {
+                  passes: 0,
+                  regressions: 0,
+                  total: 0,
+                };
+                acc.push(row);
               }
-
+              // Accumulate the sums.
+              const rs = r.legacy_status;
+              const row = acc[nodes[name]];
+              for (let i = 0; i < rs.length; i++) {
+                row.results[i].passes += rs[i].passes;
+                row.results[i].total += rs[i].total;
+              }
+              let diff = [];
+              if (this.diffResults && r.test in this.diffResults) {
+                diff = this.diffResults[r.test];
+              } else {
+                const [before, after] = rs;
+                const failingBefore = before.total - before.passes;
+                const failingAfter = after.total - after.passes;
+                diff.push(Math.max(after.passes - before.passes, 0)); // passes
+                diff.push(Math.max(failingAfter - failingBefore, 0)); // regressions
+                diff.push(after.total - before.total); // total
+              }
+              row.diff.passes += diff[0];
+              row.diff.regressions += diff[1];
+              row.diff.total += diff[2];
               return acc;
             };
           })(), [])
+          .filter(row => !this.onlyShowDifferences || Object.values(row.diff).find(i => !!i))
           // TODO(markdittmer): Is this still necessary?
           .sort(this.nodeSort);
       }
@@ -621,8 +689,13 @@ found in the LICENSE file.
       }
 
       getDiffDelta(node, prop) {
-        return node.results[1][prop || 'passes'] -
-          node.results[0][prop || 'passes'];
+        let val = 0;
+        if (!prop) {
+          val = Object.values(node.diff).forEach(v => val += Math.abs(v));
+        } else {
+          val = node.diff[prop];
+        }
+        return prop === 'regressions' ? -val : val;
       }
 
       getDiffDeltaStr(node, prop) {

shared/run_diff.go
@@ -88,8 +88,8 @@ func FetchRunResultsJSON(ctx context.Context, r *http.Request, run TestRun) (res
 	return results, nil
 }
 
-// GetResultsDiff returns a map of test name to an array of [count-different-tests, total-tests], for tests which had
-// different results counts in their map (which is test name to array of [count-passed, total-tests]).
+// GetResultsDiff returns a map of test name to an array of [newly-passing, newly-failing, total-delta], for tests which had
+// different results counts in their map (which is test name to array of [count-passed, total]).
 //
 func GetResultsDiff(before map[string][]int, after map[string][]int, filter DiffFilterParam) map[string][]int {
 	diff := make(map[string][]int)
@@ -104,22 +104,27 @@ func GetResultsDiff(before map[string][]int, after map[string][]int, filter Diff
 				if !filter.Deleted {
 					continue
 				}
-				diff[test] = []int{resultsBefore[1], resultsBefore[1]}
+				diff[test] = []int{resultsBefore[0], resultsBefore[1] - resultsBefore[0], -resultsBefore[1]}
 			} else {
 				if !filter.Changed && !filter.Unchanged {
 					continue
 				}
-				passDiff := abs(resultsBefore[0] - resultsAfter[0])
-				countDiff := abs(resultsBefore[1] - resultsAfter[1])
-				changed := passDiff != 0 || countDiff != 0
+				improved, regressed, delta := 0, 0, resultsBefore[0]-resultsAfter[0]
+				if delta < 0 {
+					improved = abs(delta)
+				} else {
+					regressed = delta
+				}
+				changed := delta != 0 || resultsBefore[1] != resultsAfter[1]
 				if (!changed && !filter.Unchanged) || changed && !filter.Changed {
 					continue
 				}
 				// Changed tests is at most the number of different outcomes,
 				// but newly introduced tests should still be counted (e.g. 0/2 => 0/5)
 				diff[test] = []int{
-					max(passDiff, countDiff),
-					max(resultsBefore[1], resultsAfter[1]),
+					improved,
+					regressed,
+					resultsAfter[1] - resultsBefore[1],
 				}
 			}
 		}
@@ -132,7 +137,7 @@ func GetResultsDiff(before map[string][]int, after map[string][]int, filter Diff
 
 			if _, ok := before[test]; !ok {
 				// Missing? Then N / N tests are 'different'
-				diff[test] = []int{resultsAfter[1], resultsAfter[1]}
+				diff[test] = []int{resultsAfter[0], resultsAfter[1] - resultsAfter[0], resultsAfter[1]}
 			}
 		}
 	}

shared/run_diff_test.go
@@ -23,44 +23,44 @@ func TestDiffResults_NoDifference(t *testing.T) {
 
 func TestDiffResults_Difference(t *testing.T) {
 	// One test now passing
-	assertDelta(t, []int{0, 1}, []int{1, 1}, []int{1, 1})
+	assertDelta(t, []int{0, 1}, []int{1, 1}, []int{1, 0, 0})
 
 	// One test now failing
-	assertDelta(t, []int{1, 1}, []int{0, 1}, []int{1, 1})
+	assertDelta(t, []int{1, 1}, []int{0, 1}, []int{0, 1, 0})
 
 	// Two tests, one now failing
-	assertDelta(t, []int{2, 2}, []int{1, 2}, []int{1, 2})
+	assertDelta(t, []int{2, 2}, []int{1, 2}, []int{0, 1, 0})
 
 	// Three tests, two now passing
-	assertDelta(t, []int{1, 3}, []int{3, 3}, []int{2, 3})
+	assertDelta(t, []int{1, 3}, []int{3, 3}, []int{2, 0, 0})
 }
 
 func TestDiffResults_Added(t *testing.T) {
 	// One new test, all passing
-	assertDelta(t, []int{1, 1}, []int{2, 2}, []int{1, 2})
+	assertDelta(t, []int{1, 1}, []int{2, 2}, []int{1, 0, 1})
 
 	// One new test, all failing
-	assertDelta(t, []int{0, 1}, []int{0, 2}, []int{1, 2})
+	assertDelta(t, []int{0, 1}, []int{0, 2}, []int{0, 0, 1})
 
 	// One new test, new test passing
-	assertDelta(t, []int{0, 1}, []int{1, 2}, []int{1, 2})
+	assertDelta(t, []int{0, 1}, []int{1, 2}, []int{1, 0, 1})
 
 	// One new test, new test failing
-	assertDelta(t, []int{1, 1}, []int{1, 2}, []int{1, 2})
+	assertDelta(t, []int{1, 1}, []int{1, 2}, []int{0, 0, 1})
 }
 
 func TestDiffResults_Removed(t *testing.T) {
 	// One removed test, all passing
-	assertDelta(t, []int{2, 2}, []int{1, 1}, []int{1, 2})
+	assertDelta(t, []int{2, 2}, []int{1, 1}, []int{0, 1, -1})
 
 	// One removed test, all failing
-	assertDelta(t, []int{0, 2}, []int{0, 1}, []int{1, 2})
+	assertDelta(t, []int{0, 2}, []int{0, 1}, []int{0, 0, -1})
 
 	// One removed test, deleted test passing
-	assertDelta(t, []int{1, 2}, []int{0, 1}, []int{1, 2})
+	assertDelta(t, []int{1, 2}, []int{0, 1}, []int{0, 1, -1})
 
 	// One removed test, deleted test failing
-	assertDelta(t, []int{1, 2}, []int{1, 1}, []int{1, 2})
+	assertDelta(t, []int{1, 2}, []int{1, 1}, []int{0, 0, -1})
 }
 
 func TestDiffResults_Filtered(t *testing.T) {
@@ -79,16 +79,16 @@ func TestDiffResults_Filtered(t *testing.T) {
 		changedPath: {3, 5},
 		addedPath:   {1, 3},
 	}
-	assert.Equal(t, map[string][]int{changedPath: {1, 5}}, GetResultsDiff(before, after, changedFilter))
-	assert.Equal(t, map[string][]int{addedPath: {3, 3}}, GetResultsDiff(before, after, addedFilter))
-	assert.Equal(t, map[string][]int{removedPath: {2, 2}}, GetResultsDiff(before, after, deletedFilter))
+	assert.Equal(t, map[string][]int{changedPath: {1, 0, 0}}, GetResultsDiff(before, after, changedFilter))
+	assert.Equal(t, map[string][]int{addedPath: {1, 2, 3}}, GetResultsDiff(before, after, addedFilter))
+	assert.Equal(t, map[string][]int{removedPath: {1, 1, -2}}, GetResultsDiff(before, after, deletedFilter))
 
 	// Test filtering by each /, /mock/, and /mock/path.html
 	pieces := strings.SplitAfter(mockTestPath, "/")
 	for i := 1; i < len(pieces); i++ {
 		paths := mapset.NewSet(strings.Join(pieces[:i], ""))
 		filter := DiffFilterParam{Changed: true, Paths: paths}
-		assertDeltaWithFilter(t, []int{0, 5}, []int{5, 5}, []int{5, 5}, filter)
+		assertDeltaWithFilter(t, []int{1, 3}, []int{2, 4}, []int{1, 0, 1}, filter)
 	}
 
 	// Filter where none match
@@ -110,7 +110,7 @@ func TestDiffResults_Filtered(t *testing.T) {
 	delta := GetResultsDiff(rBefore, rAfter, filter)
 	assert.NotContains(t, delta, mockPath2)
 	assert.Contains(t, delta, mockPath1)
-	assert.Equal(t, []int{2, 2}, delta[mockPath1])
+	assert.Equal(t, []int{2, 0, 1}, delta[mockPath1])
 }
 
 func assertNoDeltaDifferences(t *testing.T, before []int, after []int) {

util/populate_dev_data.go
@@ -201,6 +201,10 @@ func main() {
 	log.Print("Adding latest experimental TestRun data...")
 	filters.Labels = mapset.NewSetWith("experimental")
 	copyProdRuns(ctx, filters)
+
+	log.Print("Adding latest beta TestRun data...")
+	filters.Labels = mapset.NewSetWith("beta")
+	copyProdRuns(ctx, filters)
 }
 
 func copyProdRuns(ctx context.Context, filters shared.TestRunFilter) {

webapp/components/test-run.html
@@ -58,14 +58,18 @@ See models.go for more details.
         </template>
       </template>
 
-      <template is="dom-if" if="{{ !isDiff(testRun.browser_name) }}">
-        <paper-tooltip offset=0>
+      <paper-tooltip offset=0>
+        <template is="dom-if" if="{{ !isDiff(testRun.browser_name) }}">
           {{displayName(testRun.browser_name)}} {{testRun.browser_version}}<br>
           Labels: {{displayLabels(testRun.labels)}}<br>
           Started {{timeFormat(testRun.time_start)}} {{timeTaken(testRun)}}<br>
           {{moreTooltip(testRun)}}
-        </paper-tooltip>
-      </template>
+        </template>
+        <template is="dom-if" if="{{ isDiff(testRun.browser_name) }}">
+          diff numbers are for:<br>
+          [newly passing] / [newly failing] / [total count delta]
+        </template>
+      </paper-tooltip>
     </div>
   </template>
 

webapp/components/wpt-flags.html
@@ -13,7 +13,7 @@ found in the LICENSE file.
 -->
 <dom-module id="wpt-flags">
   <script>
-    const WPT_FYI_FEATURES = ['queryBuilder'];
+    const WPT_FYI_FEATURES = ['queryBuilder', 'diffFromAPI', 'diffFilter'];
 
     const _wptFlags = (superClass, readOnly) => class extends superClass {
       static get is() {
@@ -45,6 +45,16 @@ found in the LICENSE file.
     <paper-item>
       <paper-checkbox checked="{{queryBuilder}}">
         Query Builder component
+      </paper-checkbox>
+    </paper-item>
+    <paper-item>
+      <paper-checkbox checked="{{diffFromAPI}}">
+        Compute diffs using /api/diff
+      </paper-checkbox>
+    </paper-item>
+    <paper-item>
+      <paper-checkbox checked="{{diffFilter}}">
+        Filter toggle for diff view
       </paper-checkbox>
     </paper-item>
   </template>

commit 9f054524b7b7252987c7c3a179c8c6585a06407c

Fix single-item diffs (#629)
Fixes single-product views, broken by #609

webapp/components/wpt-results.html
@@ -622,20 +622,22 @@ found in the LICENSE file.
                 row.results[i].passes += rs[i].passes;
                 row.results[i].total += rs[i].total;
               }
-              let diff = [];
-              if (this.diffResults && r.test in this.diffResults) {
-                diff = this.diffResults[r.test];
-              } else {
-                const [before, after] = rs;
-                const failingBefore = before.total - before.passes;
-                const failingAfter = after.total - after.passes;
-                diff.push(Math.max(after.passes - before.passes, 0)); // passes
-                diff.push(Math.max(failingAfter - failingBefore, 0)); // regressions
-                diff.push(after.total - before.total); // total
+              if (this.diff && rs.length === 2) {
+                let diff = [];
+                if (this.diffResults && r.test in this.diffResults) {
+                  diff = this.diffResults[r.test];
+                } else {
+                  const [before, after] = rs;
+                  const failingBefore = before.total - before.passes;
+                  const failingAfter = after.total - after.passes;
+                  diff.push(Math.max(after.passes - before.passes, 0)); // passes
+                  diff.push(Math.max(failingAfter - failingBefore, 0)); // regressions
+                  diff.push(after.total - before.total); // total
+                }
+                row.diff.passes += diff[0];
+                row.diff.regressions += diff[1];
+                row.diff.total += diff[2];
               }
-              row.diff.passes += diff[0];
-              row.diff.regressions += diff[1];
-              row.diff.total += diff[2];
               return acc;
             };
           })(), [])

webdriver/product_test.go
@@ -79,6 +79,17 @@ func testProduct(
 	}
 
 	assertProducts(t, wd, testRuns, products...)
+
+	// Wait for the actual results to load.
+	resultsLoadedCondition := func(wd selenium.WebDriver) (bool, error) {
+		pathParts, err := getPathPartElements(wd, "wpt-results")
+		if err != nil {
+			return false, err
+		}
+		return len(pathParts) > 0, nil
+	}
+	err = wd.WaitWithTimeout(resultsLoadedCondition, time.Second*10)
+	assert.Nil(t, err)
 }
 
 func assertProducts(t *testing.T, wd selenium.WebDriver, testRuns []selenium.WebElement, products ...shared.ProductSpec) {