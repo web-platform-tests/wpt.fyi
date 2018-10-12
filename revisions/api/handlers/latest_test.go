b37c766f00ae0048091d2e53b74226f8c8b5a709
Mark Dittmer <markdittmer@google.com>
2018-10-12 12:01:58 -0400

    Test push package; some tests moved from handlers to push

--- revisions/api/handlers/latest_test.go
+++ revisions/api/handlers/latest_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/web-platform-tests/wpt.fyi/revisions/api/handlers"
 	"github.com/web-platform-tests/wpt.fyi/revisions/epoch"
 	agit "github.com/web-platform-tests/wpt.fyi/revisions/git"
+
 	"github.com/web-platform-tests/wpt.fyi/revisions/test"
 )
 
@@ -61,63 +62,6 @@ func TestLatestHandler_NoEpochs(t *testing.T) {
 	a.EXPECT().GetAnnouncer().Return(ancr)
 	a.EXPECT().GetEpochs().Return([]epoch.Epoch{})
 	a.EXPECT().ErrorJSON(icMatcher{"epochs"}).Return("")
-
-	req := httptest.NewRequest("GET", "/api/revisions/latest", new(strings.Reader))
-	resp := httptest.NewRecorder()
-
-	handlers.LatestHandler(a, resp, req)
-
-	assert.Equal(t, http.StatusInternalServerError, resp.Code)
-}
-
-func TestLatestHandler_FailedGetRevisions(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	a := api.NewMockAPI(mockCtrl)
-	ancr := announcer.NewMockAnnouncer(mockCtrl)
-
-	epochs := []epoch.Epoch{epoch.Hourly{}}
-	latestInput := make(map[epoch.Epoch]int)
-	err := errors.New("GetRevisions error")
-	a.EXPECT().GetAnnouncer().Return(ancr)
-	a.EXPECT().GetEpochs().Return(epochs)
-	a.EXPECT().GetLatestGetRevisionsInput().Return(latestInput)
-	ancr.EXPECT().GetRevisions(latestInput, gomock.Any()).Return(nil, err)
-	a.EXPECT().ErrorJSON(icMatcher{"getrevisions"}).Return("")
-
-	req := httptest.NewRequest("GET", "/api/revisions/latest", new(strings.Reader))
-	resp := httptest.NewRecorder()
-
-	handlers.LatestHandler(a, resp, req)
-
-	assert.Equal(t, http.StatusInternalServerError, resp.Code)
-}
-
-func TestLatestHandler_FailedLatestFromEpochs(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	a := api.NewMockAPI(mockCtrl)
-	ancr := announcer.NewMockAnnouncer(mockCtrl)
-
-	epochs := []epoch.Epoch{epoch.Hourly{}}
-	latestInput := make(map[epoch.Epoch]int)
-
-	// Empty list filed under epoch triggers error in LatestFromEpochs.
-	//
-	// TODO(markdittmer): Perhaps functions in revisions/api/types.go should be
-	// wrapped in an interface or struct so that they can be mocked.
-	revs := map[epoch.Epoch][]agit.Revision{
-		epoch.Hourly{}: []agit.Revision{},
-	}
-
-	a.EXPECT().GetAnnouncer().Return(ancr)
-	a.EXPECT().GetEpochs().Return(epochs)
-	a.EXPECT().GetLatestGetRevisionsInput().Return(latestInput)
-
-	ancr.EXPECT().GetRevisions(latestInput, gomock.Any()).Return(revs, nil)
-	a.EXPECT().ErrorJSON(icMatcher{"missing"}).Return("")
 
 	req := httptest.NewRequest("GET", "/api/revisions/latest", new(strings.Reader))
 	resp := httptest.NewRecorder()
