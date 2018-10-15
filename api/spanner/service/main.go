af2f943e951d4859c33291f6ade7a6b5afc652e7
Mark Dittmer <mdittmer@users.noreply.github.com>
2018-10-15 16:05:42 -0400

    Spanner push run: Compare result counts (#658)
    This implements the initial check for pushing a run to Cloud Spanner: Does the report contain more distinct test results than Cloud Spanner? If `reportResultCount > spannerRowCount` for the given run ID, then (in a follow-up PR) the service will write all results to Cloud Spanner.

--- api/spanner/service/main.go
+++ api/spanner/service/main.go
@@ -16,9 +16,21 @@ import (
 	"github.com/web-platform-tests/wpt.fyi/shared"
 )
 
+const (
+	// Database (unlike project ID) is const because a consistent schema is
+	// maintained for same-name databases across projects. If schema changes are
+	// needed, usually a new database is created, then code containing assumptions
+	// about schema can be updated alongside this constant in a single change.
+	spannerDatabase = "results-apep"
+)
+
 var (
-	port = flag.Int("port", 8080, "Port to listen on")
-	auth spanner.Authenticator
+	port               = flag.Int("port", 8080, "Port to listen on")
+	projectID          = flag.String("project_id", "", "Google Cloud Platform project ID, if different from ID detected from metadata service")
+	spannerInstance    = flag.String("spanner_instance", "wpt-results", "Cloud Spanner instance ID where data are stored")
+	gcpCredentialsFile = flag.String("gcp_credentials_file", "", "Path to Google Cloud Platform credentials file, if necessary")
+	auth               spanner.Authenticator
+	api                spanner.API
 )
 
 func livenessCheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,7 +42,7 @@ func readinessCheckHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func spannerPushRunHandler(w http.ResponseWriter, r *http.Request) {
-	spanner.HandlePushRun(shared.NewRequestContext(r), auth, w, r)
+	spanner.HandlePushRun(shared.NewRequestContext(r), api, w, r)
 }
 
 func init() {
@@ -38,13 +50,25 @@ func init() {
 }
 
 func main() {
-	projectID, err := metadata.ProjectID()
+	autoProjectID, err := metadata.ProjectID()
 	if err != nil {
 		log.Warningf("Failed to get project ID from metadata service; disabling spanner service authentication")
 		auth = spanner.NewNopAuthenticator()
 	} else {
-		log.Infof(`Using project ID from metadata service: "%s"`, projectID)
-		auth = spanner.NewDatastoreAuthenticator(projectID)
+		if *projectID == "" {
+			log.Infof(`Using project ID from metadata service: "%s"`, *projectID)
+			*projectID = autoProjectID
+		} else if *projectID != autoProjectID {
+			log.Warningf(`Using project ID from flag: "%s" even though metadata service reports project ID of "%s"`, *projectID, autoProjectID)
+		} else {
+			log.Infof(`Using project ID: "%s"`, *projectID)
+		}
+		auth = spanner.NewDatastoreAuthenticator(*projectID)
+	}
+
+	api = spanner.NewAPI(auth, *projectID, *spannerInstance, spannerDatabase)
+	if *gcpCredentialsFile != "" {
+		api = api.WithCredentialsFile(*gcpCredentialsFile)
 	}
 
 	http.HandleFunc("/_ah/liveness_check", livenessCheckHandler)
