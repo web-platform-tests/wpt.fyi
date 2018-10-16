0b1d558b4015b7383c4a54e4c0e061ae8fc0f8d2
Mark Dittmer <markdittmer@google.com>
2018-10-16 14:40:14 -0400

    Implement spanner push run
    - Don't bother if numResults(report) <= numResults(spanner)
    - Use 5 retries around each major step
    - Push up to 1000 batches of 1000 mutations concurrently
    - Push test (ID, name, subtest name) definitions first, then results
      - Ensures consistency using results as sub-table of tests
    - Log warning on retryable failure
    - Log error on push-breaking failure

--- api/spanner/push_run.go
+++ api/spanner/push_run.go
@@ -10,21 +10,29 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"strconv"
 	"time"
 
 	"cloud.google.com/go/datastore"
 	"cloud.google.com/go/spanner"
+	retry "github.com/avast/retry-go"
 	mapset "github.com/deckarep/golang-set"
+	farm "github.com/dgryski/go-farm"
 	log "github.com/sirupsen/logrus"
 	"github.com/web-platform-tests/results-analysis/metrics"
 	"github.com/web-platform-tests/wpt.fyi/shared"
+	"golang.org/x/sync/semaphore"
 	"google.golang.org/api/iterator"
 )
 
 const (
-	countStmt = "SELECT COUNT(*) FROM results WHERE run_id = @run_id"
+	numConcurrentBatches = 1000
+	batchSize            = 1000
+	testsTableName       = "tests"
+	resultsTableName     = "results"
+	countStmt            = "SELECT COUNT(*) FROM results WHERE run_id = @run_id"
 )
 
 // PushID is a unique identifier for a request to push a test run to
@@ -73,38 +81,123 @@ func pushRun(ctx context.Context, api API, id int64) {
 		dsClient *datastore.Client
 		sClient  *spanner.Client
 		err      error
+		logger   = shared.GetLogger(ctx)
 	)
 	dsClient, err = api.DatastoreConnect(ctx)
 	if err != nil {
-		shared.GetLogger(ctx).Errorf("Spanner push run failed connecting to datastore: %v", err)
+		logger.Errorf("Spanner push run failed connecting to datastore: %v", err)
 		return
 	}
 
 	run, err := loadRun(ctx, dsClient, id)
 	if err != nil {
-		shared.GetLogger(ctx).Errorf("Spanner push run failed loading run: %v", err)
+		logger.Errorf("Spanner push run failed loading run: %v", err)
 		return
 	}
 
 	report, err := loadRunReport(ctx, run)
 	if err != nil {
-		shared.GetLogger(ctx).Errorf("Spanner push run failed loading run report: %v", err)
+		logger.Errorf("Spanner push run failed loading run report: %v", err)
 		return
 	}
 
 	sClient, err = api.SpannerConnect(ctx)
 	if err != nil {
-		shared.GetLogger(ctx).Errorf("Spanner push run failed connecting to spanner: %v", err)
+		logger.Errorf("Spanner push run failed connecting to spanner: %v", err)
 		return
 	}
 
-	n, err := numRowsToUpload(ctx, sClient, id, report)
+	// Determine number of rows missing in spanner.
+	// Retries: 5.
+	var n int64
+	retry.Do(func() error {
+		n, err = numRowsToUpload(ctx, sClient, run.ID, report)
+		return err
+	}, retry.Attempts(5), retry.OnRetry(func(n uint, err error) {
+		logger.Warningf("Attempt #%d to get number of rows to push failed: %v", n, err)
+	}))
 	if err != nil {
-		shared.GetLogger(ctx).Errorf("Spanner push run failed calculating number of rows to upload: %v", err)
+		logger.Errorf("Spanner push run failed calculating number of rows to upload: %v", err)
+		return
+	}
+	if n < 0 {
+		logger.Warningf("Spanner contains %d more rows than WPT report", -n)
+		return
+	}
+	if n == 0 {
+		logger.Infof("Run already uploaded to spanner")
 		return
 	}
 
-	shared.GetLogger(ctx).Infof("NOT IMPLEMENTED: Spanner push run would now push run if number of missing rows=%d > 0", n)
+	// Generate mutations for complete run.
+	logger.Infof("Queuing rows for run")
+	tests, results := getMutationsForRun(run.ID, report)
+	logger.Infof("Queued %d+%d rows", len(tests), len(results))
+
+	// Write rows to spanner in batches.
+	// Retries: 5 per batch.
+	writeMutations := func(muts []*spanner.Mutation) []error {
+		logger.Infof("Writing %d-row batches", len(muts))
+		s := semaphore.NewWeighted(numConcurrentBatches)
+		ec := make(chan error, int64(math.Ceil(float64(len(muts))/float64(batchSize))))
+		writeBatch := func(m, n int) error {
+			batch := muts[m:n]
+
+			logger.Infof("Writing batch: [%d,%d)", m, n)
+			_, err := sClient.Apply(ctx, batch)
+			if err != nil {
+				return err
+			}
+			logger.Infof("Wrote batch: [%d,%d)", m, n)
+			return nil
+		}
+		retryableWriteBatch := func(m, n int) {
+			defer s.Release(1)
+			err := retry.Do(func() error {
+				return writeBatch(m, n)
+			}, retry.Attempts(5), retry.OnRetry(func(num uint, err error) {
+				logger.Warningf("Attempt #%d to write batch [%d,%d) failed: %v", num, m, n, err)
+			}))
+			if err != nil {
+				ec <- err
+			}
+		}
+		var end int
+		for end = batchSize; end <= len(muts); end += batchSize {
+			s.Acquire(ctx, 1)
+			go retryableWriteBatch(end-batchSize, end)
+		}
+		// Corner case: Leftover rows when len(muts) % batchSize != 0.
+		if end != len(muts) {
+			s.Acquire(ctx, 1)
+			logger.Infof("Writing small batch: [%d,%d)", end-batchSize, len(muts))
+			go retryableWriteBatch(end-batchSize, len(muts))
+			logger.Infof("Wrote small batch: [%d,%d)", end-batchSize, len(muts))
+		}
+		s.Acquire(ctx, numConcurrentBatches)
+
+		errs := make([]error, 0)
+		close(ec)
+		for err := range ec {
+			logger.Errorf("Failed to write batch: %v", err)
+			errs = append(errs, err)
+		}
+		return errs
+	}
+
+	// Write tests (parent table), then results (child table).
+	errs := writeMutations(tests)
+	if len(errs) != 0 {
+		logger.Errorf("Spanner push run failed with %d test batch write errors", len(errs))
+		return
+	}
+	errs = writeMutations(results)
+	if len(errs) != 0 {
+		logger.Errorf("Spanner push run failed with %d result batch write errors", len(errs))
+		return
+	}
+
+	logger.Infof("Spanner push run succeeded: Wrote batches for %d+%d rows", len(tests), len(results))
 }
 
 // loadRun loads shared.TestRun data from datastore, given an integral ID
@@ -239,7 +332,71 @@ func numRowsToUpload(ctx context.Context, client *spanner.Client, runID int64, r
 		return 0, err
 	}
 
-	shared.GetLogger(ctx).Infof("Run %d contains %d rows (according to GCS); %d found in Spanner", runID, totalRows, existingRows)
+	shared.GetLogger(ctx).Infof("Run %d contains %d results (according to GCS); %d found in Spanner", runID, totalRows, existingRows)
 
 	return totalRows - existingRows, nil
+}
+
+// getMutationsForRun returns (test table mutations, results table mutations)
+// associated with tests and test results contained in report.
+func getMutationsForRun(runID int64, report *metrics.TestResultsReport) ([]*spanner.Mutation, []*spanner.Mutation) {
+	testsRows := make([]*spanner.Mutation, 0)
+	resultsRows := make([]*spanner.Mutation, 0)
+	for _, r := range report.Results {
+		if len(r.Subtests) == 0 {
+			testsRows = appendTestsRow(r.Test, nil, testsRows)
+			resultsRows = appendResultsRow(runID, r.Status, r.Test, nil, r.Message, resultsRows)
+		} else {
+			for _, s := range r.Subtests {
+				testsRows = appendTestsRow(r.Test, &s.Name, testsRows)
+				resultsRows = appendResultsRow(runID, s.Status, r.Test, &s.Name, s.Message, resultsRows)
+			}
+		}
+	}
+	return testsRows, resultsRows
+}
+
+// appendTestsRow appends a *spanner.Mutation to rows, containing the mutation
+// required to ensure that the associated (sub)test is stored in Cloud Spanner.
+func appendTestsRow(test string, subtest *string, rows []*spanner.Mutation) []*spanner.Mutation {
+	testID := computeTestID(test, subtest)
+	testsRowMap := map[string]interface{}{
+		"test_id": testID,
+		"test":    test,
+	}
+	if subtest != nil && *subtest != "" {
+		testsRowMap["subtest"] = *subtest
+	} else {
+		testsRowMap["subtest"] = spanner.NullString{}
+	}
+
+	// Use InsertOrUpdate to ensure provided fields are correct without triggering
+	// a Delete that could break sub-table consistency.
+	return append(rows, spanner.InsertOrUpdateMap(testsTableName, testsRowMap))
+}
+
+// appendResultsRow appends a *spanner.Mutation to rows, containing the mutation
+// required to ensure that the associated test result is stored in Cloud
+// Spanner.
+func appendResultsRow(runID int64, status, test string, subtest, message *string, rows []*spanner.Mutation) []*spanner.Mutation {
+	testID := computeTestID(test, subtest)
+	resultsRowMap := map[string]interface{}{
+		"test_id": testID,
+		"run_id":  runID,
+		"result":  shared.TestStatusValueFromString(status),
+	}
+	if message != nil && *message != "" {
+		resultsRowMap["message"] = *message
+	}
+
+	// Use Replace to clobber any existing row.
+	return append(rows, spanner.ReplaceMap(resultsTableName, resultsRowMap))
+}
+
+// computeTestID computes a stable int64 ID for a test+(optional)subtest pair.
+func computeTestID(test string, subtest *string) int64 {
+	if subtest != nil && *subtest != "" {
+		return int64(farm.Fingerprint64([]byte(test + "\x00" + *subtest)))
+	}
+	return int64(farm.Fingerprint64([]byte(test)))
 }
