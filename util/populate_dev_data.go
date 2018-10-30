6456961adfa7a319a6ca089f5087b5790f08868a
Mark Dittmer <markdittmer@google.com>
2018-10-30 14:44:08 -0400

    Add flag for structured queries

--- util/populate_dev_data.go
+++ util/populate_dev_data.go
@@ -174,6 +174,7 @@ func main() {
 	addFlag(ctx, "queryBuilder", enabledFlag)
 	addFlag(ctx, "diffFilter", enabledFlag)
 	addFlag(ctx, "diffFromAPI", enabledFlag)
+	addFlag(ctx, "structuredQueries", enabledFlag)
 
 	log.Print("Adding uploader \"test\"...")
 	addData(ctx, "Uploader", []interface{}{
