4aa53430bff92348c87e1e60c87a9f30bb11ad2e
Mark Dittmer <markdittmer@google.com>
2018-11-27 15:37:01 -0500

    Fix lint

--- api/query/cache/index/index_test.go
+++ api/query/cache/index/index_test.go
@@ -1,3 +1,5 @@
+// +build small
+
 // Copyright 2018 The WPT Dashboard Project. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
