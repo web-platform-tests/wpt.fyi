20bd16337876bdf46a0166b47e483c9ce4db8284
Luke Bjerring <lukebjerring@users.noreply.github.com>
2018-11-28 13:57:51 -0500

    Complete created/rerequested check_run via the TaskQueue (#800)

---
+++ api/checks/webhook_test.go
@@ -0,0 +1,104 @@
+// +build small
+
+// Copyright 2018 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package checks
+
+import (
+	"context"
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/google/go-github/github"
+	"github.com/stretchr/testify/assert"
+	"github.com/web-platform-tests/wpt.fyi/shared/sharedtest"
+)
+
+func TestHandleCheckRunEvent_InvalidApp(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	id := int64(123)
+	chrome := "chrome"
+	event := github.CheckRunEvent{
+		CheckRun: &github.CheckRun{
+			App: &github.App{
+				ID: &id,
+			},
+			Name: &chrome,
+		},
+	}
+	payload, _ := json.Marshal(event)
+
+	aeAPI := sharedtest.NewMockAppEngineAPI(mockCtrl)
+	aeAPI.EXPECT().Context().AnyTimes().Return(context.Background())
+	suitesAPI := NewMockSuitesAPI(mockCtrl)
+
+	processed, err := handleCheckRunEvent(aeAPI, suitesAPI, payload)
+	assert.Nil(t, err)
+	assert.False(t, processed)
+}
+
+func TestHandleCheckRunEvent_Created_Complete(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	id := int64(wptfyiCheckAppID)
+	chrome := "chrome"
+	created := "created"
+	completed := "completed"
+	event := github.CheckRunEvent{
+		Action: &created,
+		CheckRun: &github.CheckRun{
+			App: &github.App{
+				ID: &id,
+			},
+			Name:   &chrome,
+			Status: &completed,
+		},
+	}
+	payload, _ := json.Marshal(event)
+
+	aeAPI := sharedtest.NewMockAppEngineAPI(mockCtrl)
+	aeAPI.EXPECT().Context().AnyTimes().Return(context.Background())
+	suitesAPI := NewMockSuitesAPI(mockCtrl)
+
+	processed, err := handleCheckRunEvent(aeAPI, suitesAPI, payload)
+	assert.Nil(t, err)
+	assert.False(t, processed)
+}
+
+func TestHandleCheckRunEvent_Created_Pending(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	id := int64(wptfyiCheckAppID)
+	sha := strings.Repeat("0123456789", 4)
+	chrome := "chrome"
+	created := "created"
+	pending := "pending"
+	event := github.CheckRunEvent{
+		Action: &created,
+		CheckRun: &github.CheckRun{
+			App: &github.App{
+				ID: &id,
+			},
+			Name:    &chrome,
+			Status:  &pending,
+			HeadSHA: &sha,
+		},
+	}
+	payload, _ := json.Marshal(event)
+
+	aeAPI := sharedtest.NewMockAppEngineAPI(mockCtrl)
+	aeAPI.EXPECT().Context().AnyTimes().Return(context.Background())
+	suitesAPI := NewMockSuitesAPI(mockCtrl)
+	suitesAPI.EXPECT().ScheduleResultsProcessing(sha, sharedtest.SameProductSpec("chrome"))
+
+	processed, err := handleCheckRunEvent(aeAPI, suitesAPI, payload)
+	assert.Nil(t, err)
+	assert.True(t, processed)
+}
