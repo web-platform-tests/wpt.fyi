3d2045f5b2b1a4275bf7d20328ef9f6e00991cca
Luke Bjerring <lukebjerring@google.com>
2018-11-29 11:11:38 -0500

    Fix regression diff order

--- api/checks/summaries/compile_test.go
+++ api/checks/summaries/compile_test.go
@@ -7,9 +7,11 @@
 package summaries
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/web-platform-tests/wpt.fyi/shared"
 )
 
 func TestGetSummary_Completed(t *testing.T) {
@@ -36,4 +38,40 @@ func TestGetSummary_Pending(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Contains(t, s, foo.HostName)
 	assert.Contains(t, s, foo.RunsURL)
+}
+
+func TestGetSummary_Regressed(t *testing.T) {
+	master := shared.TestRun{}
+	master.Revision = "abcdef0123"
+	master.FullRevisionHash = strings.Repeat(master.Revision, 4)
+	pr := shared.TestRun{}
+	pr.Revision = "0123456789"
+	pr.FullRevisionHash = strings.Repeat(master.Revision, 4)
+	foo := Regressed{
+		MasterRun:     master,
+		PRRun:         pr,
+		HostName:      "foo.com",
+		HostURL:       "https://foo.com/",
+		DiffURL:       "https://foo.com/?products=chrome@0000000000,chrome@0123456789&diff",
+		MasterDiffURL: "https://foo.com/?products=chrome[master],chrome@0123456789&diff",
+		Regressions: map[string]BeforeAndAfter{
+			"/foo.html": BeforeAndAfter{
+				PassingBefore: 1,
+				TotalBefore:   1,
+				PassingAfter:  0,
+				TotalAfter:    1,
+			},
+		},
+		More: 1,
+	}
+	s, err := foo.GetSummary()
+	assert.Nil(t, err)
+	assert.Contains(t, s, foo.HostName)
+	assert.Contains(t, s, foo.HostURL)
+	assert.Contains(t, s, foo.DiffURL)
+	assert.Contains(t, s, master.String())
+	assert.Contains(t, s, pr.String())
+	assert.Contains(t, s, "0 / 1")
+	assert.Contains(t, s, "1 / 1")
+	assert.Contains(t, s, "And 1 others...")
 }
--- api/checks/summaries/regressed.md
+++ api/checks/summaries/regressed.md
@@ -1,8 +1,15 @@
 Results have successfully been scraped and added to [{{ .HostName }}]({{ .HostURL }}).
 
-Uh-oh - it looks like there are some newly-failing results.
+Uh-oh - it looks like there are some newly-failing results when we compared the affected tests
+to the latest run against the `master` branch.
 
-Test | `master` | `{{ printf "%.7s" .HeadSHA }}`
+Run | Spec
+`master` | [{{ .MasterRun.String }}]
+`{{ printf "%.7s" .PRRun.FullRevisionHash }}` | {{ .PRRun.String }}
+
+### Regressions
+
+Test | `master` | `{{ printf "%.7s" .PRRun.FullRevisionHash }}`
 --- | --- | ---
 {{ range $test, $results := .Regressions -}}
 {{ $test }} | {{ $results.PassingBefore }} / {{ $results.TotalBefore }} | {{ $results.PassingAfter }} / {{ $results.TotalAfter }}
@@ -10,5 +17,10 @@ Test | `master` | `{{ printf "%.7s" .HeadSHA }}`
 {{ if gt .More 0 -}}
 And {{ .More }} others...
 {{ end }}
-A visual comparison of the results against `master` run:
-[This PR vs master]({{ .DiffURL }})
+
+You can view a visual comparison of all the results [here]]({{ .DiffURL }}).
+
+Other views that might be useful:
+- [`{{ printf "%.7s" .PRRun.FullRevisionHash }}` vs `master`@`{{ printf "%.7s" .MasterRun.FullRevisionHash }}`]({{ .DiffURL }})
+- [`{{ printf "%.7s" .PRRun.FullRevisionHash }}` vs latest master]({{ .MasterDiffURL }})
+- [Latest results for `{{ printf "%.7s" .PRRun.FullRevisionHash }}`]({{.HostURL}}?sha={{.PRRun.Revision}})
