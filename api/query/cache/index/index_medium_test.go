7883fa6b2ce7d32e5424aa480a7261f5c979dc21
Mark Dittmer <markdittmer@google.com>
2018-11-29 14:17:18 -0500

    Test & implement search cache index stores all run+test+result data
    - `Index` stores `Tests` and shards of `Results`
    - `Index` carefully manages repeated and/or concurrent attempts to add same run
    - Test new `Index` functionality + medium test for "run eviction relieves memory pressure"

---
+++ api/query/cache/index/index_medium_test.go
@@ -0,0 +1,62 @@
+// +build medium
+
+// Copyright 2018 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package index
+
+import (
+	"runtime"
+	"strconv"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	metrics "github.com/web-platform-tests/results-analysis/metrics"
+	"github.com/web-platform-tests/wpt.fyi/shared"
+)
+
+const (
+	testEvictionNumResults = 10000
+	// Each result should require at least one byte for its name + 4 bytes for
+	// "PASS".
+	testEvictionMinBytes = testEvictionNumResults * 5
+)
+
+func TestEvictAnyRunRelievesMemoryPressure(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	loader := NewMockReportLoader(ctrl)
+	i, err := NewShardedWPTIndex(loader, 1)
+	assert.Nil(t, err)
+	run := shared.TestRun{
+		ID:            1,
+		RawResultsURL: "http://example.com/results.json",
+	}
+	results := make([]*metrics.TestResults, 0, testEvictionNumResults)
+	for j := 0; j < testEvictionNumResults; j++ {
+		str := strconv.Itoa(j)
+		results = append(results, &metrics.TestResults{
+			Test:   str,
+			Status: "PASS",
+		})
+	}
+	report := &metrics.TestResultsReport{Results: results}
+	loader.EXPECT().Load(run).Return(report, nil)
+
+	assert.Nil(t, i.IngestRun(run))
+	results = nil
+	runtime.GC()
+
+	var stats runtime.MemStats
+	runtime.GC()
+	runtime.ReadMemStats(&stats)
+	baseline := stats.HeapAlloc
+
+	assert.Nil(t, i.EvictAnyRun())
+
+	runtime.GC()
+	runtime.ReadMemStats(&stats)
+	assert.True(t, baseline-testEvictionMinBytes > stats.HeapAlloc)
+}
