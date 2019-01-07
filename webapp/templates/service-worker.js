9ad28c9b426fd54717dae54385d072855773fd8c
Luke Bjerring <lukebjerring@google.com>
2019-01-07 15:51:03 -0500

    Ignore non-ok, clear old caches

--- webapp/templates/service-worker.js
+++ webapp/templates/service-worker.js
@@ -1,3 +1,19 @@
+// Clean up old caches.
+self.addEventListener(
+  'activate',
+  e => {
+    e.waitUntil(
+      caches.keys()
+        .then(cacheNames => Promise.all(
+          cacheNames
+            .filter(v => '{{ .Version }}' !== v)
+            .map(v => caches.delete(v))
+        ))
+    );
+  }
+);
+
+// Locally cache eligible components/files.
 self.addEventListener(
   'fetch',
   e => {
@@ -9,12 +25,14 @@ self.addEventListener(
           }
           return fetch(e.request)
             .then(r => {
-              const components = new RegExp('^/((bower_)?components|static)/');
-              if (components.test(e.request.url.toString())) {
-                let clone = r.clone();
-                caches.open('{{ .Version }}').then(cache => {
-                  cache.put(e.request, clone);
-                });
+              if (r.ok) {
+                const components = new RegExp('^/((bower_)?components|static)/');
+                if (components.test(e.request.url.toString())) {
+                  let clone = r.clone();
+                  caches.open('{{ .Version }}').then(cache => {
+                    cache.put(e.request, clone);
+                  });
+                }
               }
               return r;
             });
