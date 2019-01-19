web-platform-tests/wpt.fyi

commit 27a6abd2ddf24f781dd7f4feb704c897ac764aea

Handle multiple report file (shards) in azure check webhook (#1022)

api/azure/api.go
@@ -9,9 +9,12 @@ import (
 	"bytes"
 	"compress/gzip"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	multipart "mime/multipart"
+	"regexp"
 	"time"
 
 	"github.com/google/go-github/github"
@@ -81,7 +84,7 @@ func (a apiImpl) GetAzureArtifactsURL(owner, repo string, buildID int64) string 
 
 // FetchAzureArtifact gets the gzipped bytes of the wpt_report.json from inside
 // the zip file provided by Azure, and writes them to the given writer.
-func (a apiImpl) FetchAzureArtifact(artifact BuildArtifact, w io.Writer) error {
+func (a apiImpl) FetchAzureArtifact(artifact BuildArtifact, writer io.Writer) error {
 	aeAPI := shared.NewAppEngineAPI(a.ctx)
 	log := shared.GetLogger(a.ctx)
 	// The default timeout is 5s, not enough to download the reports.
@@ -96,36 +99,59 @@ func (a apiImpl) FetchAzureArtifact(artifact BuildArtifact, w io.Writer) error {
 		return err
 	}
 
-	// Extract the report from the artifact.
-	reportPath := fmt.Sprintf("%s/wpt_report.json", artifact.Name)
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Errorf("Failed to read response body")
 		return err
 	}
+
+	// Extract the report from the artifact.
+	return extractReports(a.ctx, artifact.Name, data, writer)
+}
+
+// extractReports extracts report files from the given zip.
+func extractReports(ctx context.Context, artifactName string, data []byte, writer io.Writer) error {
+	log := shared.GetLogger(ctx)
+	reportPath, err := regexp.Compile(fmt.Sprintf(`%s/wpt_report.*\.json$`, artifactName))
+	if err != nil {
+		return err
+	}
+	extracted := 0
+	mWriter := multipart.NewWriter(writer)
 	z, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
 	for _, f := range z.File {
-		if f.Name == reportPath {
-			var fileData io.ReadCloser
-			if fileData, err = f.Open(); err != nil {
-				log.Errorf("Failed to extract %s", reportPath)
-				return err
-			}
-			defer fileData.Close()
+		if reportPath.MatchString(f.Name) {
+			// Wrap extraction in a function, to scope the "defer fileData.Close()"
+			if err := func() error {
+				fileName := f.Name[len(artifactName)+1:]
+				fileField, err := mWriter.CreateFormFile("result_file", fileName)
+				var fileData io.ReadCloser
+				if fileData, err = f.Open(); err != nil {
+					log.Errorf("Failed to extract %s", f.Name)
+					return err
+				}
+				defer fileData.Close()
 
-			gzw := gzip.NewWriter(w)
-			if _, err := io.Copy(gzw, fileData); err != nil {
-				log.Errorf("Failed to gzip file contents")
+				gzw := gzip.NewWriter(fileField)
+				if _, err := io.Copy(gzw, fileData); err != nil {
+					log.Errorf("Failed to gzip file contents")
+					return err
+				}
+				if err := gzw.Close(); err != nil {
+					log.Errorf("Failed to close gzip writer")
+					return err
+				}
+				return nil
+			}(); err != nil {
 				return err
 			}
-			if err := gzw.Close(); err != nil {
-				log.Errorf("Failed to close gzip writer")
-				return err
-			}
-			return nil
+			extracted++
 		}
 	}
-	return fmt.Errorf("File %s not found in zip", reportPath)
+	if extracted < 1 {
+		return errors.New(`No "wpt_report.*\.json" files found in zip`)
+	}
+	return mWriter.Close()
 }
 
 func getCheckTitle(product shared.ProductSpec) string {

api/azure/webhook.go
@@ -128,11 +128,9 @@ func createAzureRun(
 	host := aeAPI.GetVersionedHostname()
 	writer.WriteField("callback_url", fmt.Sprintf("https://%s/api/results/create", host))
 
-	fileField, err := writer.CreateFormFile("result_file", "wpt_report.json")
-	if err = azureAPI.FetchAzureArtifact(artifact, fileField); err != nil {
+	if err := azureAPI.FetchAzureArtifact(artifact, buf); err != nil {
 		return err
 	}
-
 	if err := writer.Close(); err != nil {
 		return err
 	}

api/azure/api_test.go
@@ -1,0 +1,36 @@
+// +build small
+
+// Copyright 2018 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package azure
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"path"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExtractFiles(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	// artifact_test.zip has a single dir, artifact_test/, containing 2 files, wpt_report_{1,2}.json
+	data, err := ioutil.ReadFile(path.Join(path.Dir(filename), "artifact_test.zip"))
+	if err != nil {
+		assert.FailNow(t, "Failed to read artifact_test.zip", err.Error())
+	}
+	buf := new(bytes.Buffer)
+	err = extractReports(context.Background(), "artifact_test", data, buf)
+	if err != nil {
+		assert.FailNow(t, "Failed to extract reports", err.Error())
+	}
+	assert.Nil(t, err)
+	content := string(buf.Bytes())
+	assert.Contains(t, content, "wpt_report_1.json")
+	assert.Contains(t, content, "wpt_report_2.json")
+}

webapp/app.yaml
@@ -42,3 +42,4 @@ skip_files:
 - .eslintrc.json
 - package.json
 - README.md
+- .*/artifact_test.zip

commit 12d93e2b41c46a70fedb12159db2cdf29dbb71e0

Fix the result collection in the Azure webhook
Bug was introduced in
https://github.com/web-platform-tests/wpt.fyi/pull/1022 because of an
incorrect suggestion from me. This PR fixes the bug by reusing the
same multipart.Writer throughout a request.

This PR also improves error logging in the results receiver to help
future debugging.

api/azure/api.go
@@ -51,7 +51,7 @@ type ArtifactResource struct {
 type API interface {
 	HandleCheckRunEvent(*github.CheckRunEvent) (bool, error)
 	GetAzureArtifactsURL(owner, repo string, buildID int64) string
-	FetchAzureArtifact(BuildArtifact, io.Writer) error
+	FetchAzureArtifact(BuildArtifact, *multipart.Writer) error
 }
 
 type apiImpl struct {
@@ -84,7 +84,7 @@ func (a apiImpl) GetAzureArtifactsURL(owner, repo string, buildID int64) string 
 
 // FetchAzureArtifact gets the gzipped bytes of the wpt_report.json from inside
 // the zip file provided by Azure, and writes them to the given writer.
-func (a apiImpl) FetchAzureArtifact(artifact BuildArtifact, writer io.Writer) error {
+func (a apiImpl) FetchAzureArtifact(artifact BuildArtifact, mWriter *multipart.Writer) error {
 	aeAPI := shared.NewAppEngineAPI(a.ctx)
 	log := shared.GetLogger(a.ctx)
 	// The default timeout is 5s, not enough to download the reports.
@@ -106,18 +106,17 @@ func (a apiImpl) FetchAzureArtifact(artifact BuildArtifact, writer io.Writer) er
 	}
 
 	// Extract the report from the artifact.
-	return extractReports(a.ctx, artifact.Name, data, writer)
+	return extractReports(a.ctx, artifact.Name, data, mWriter)
 }
 
 // extractReports extracts report files from the given zip.
-func extractReports(ctx context.Context, artifactName string, data []byte, writer io.Writer) error {
+func extractReports(ctx context.Context, artifactName string, data []byte, mWriter *multipart.Writer) error {
 	log := shared.GetLogger(ctx)
 	reportPath, err := regexp.Compile(fmt.Sprintf(`%s/wpt_report.*\.json$`, artifactName))
 	if err != nil {
 		return err
 	}
 	extracted := 0
-	mWriter := multipart.NewWriter(writer)
 	z, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
 	for _, f := range z.File {
 		if reportPath.MatchString(f.Name) {

api/azure/api_mock.go
@@ -1,15 +1,14 @@
 // Code generated by MockGen. DO NOT EDIT.
-// Source: github.com/web-platform-tests/wpt.fyi/api/azure (interfaces: API)
+// Source: api/azure/api.go
 
-// Package azure is a generated GoMock package.
-package azure
+// Package mock_azure is a generated GoMock package.
+package mock_azure
 
 import (
-	io "io"
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
 	github "github.com/google/go-github/github"
+	multipart "mime/multipart"
+	reflect "reflect"
 )
 
 // MockAPI is a mock of API interface
@@ -35,32 +34,9 @@ func (m *MockAPI) EXPECT() *MockAPIMockRecorder {
 	return m.recorder
 }
 
-// FetchAzureArtifact mocks base method
-func (m *MockAPI) FetchAzureArtifact(arg0 BuildArtifact, arg1 io.Writer) error {
-	ret := m.ctrl.Call(m, "FetchAzureArtifact", arg0, arg1)
-	ret0, _ := ret[0].(error)
-	return ret0
-}
-
-// FetchAzureArtifact indicates an expected call of FetchAzureArtifact
-func (mr *MockAPIMockRecorder) FetchAzureArtifact(arg0, arg1 interface{}) *gomock.Call {
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FetchAzureArtifact", reflect.TypeOf((*MockAPI)(nil).FetchAzureArtifact), arg0, arg1)
-}
-
-// GetAzureArtifactsURL mocks base method
-func (m *MockAPI) GetAzureArtifactsURL(arg0, arg1 string, arg2 int64) string {
-	ret := m.ctrl.Call(m, "GetAzureArtifactsURL", arg0, arg1, arg2)
-	ret0, _ := ret[0].(string)
-	return ret0
-}
-
-// GetAzureArtifactsURL indicates an expected call of GetAzureArtifactsURL
-func (mr *MockAPIMockRecorder) GetAzureArtifactsURL(arg0, arg1, arg2 interface{}) *gomock.Call {
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAzureArtifactsURL", reflect.TypeOf((*MockAPI)(nil).GetAzureArtifactsURL), arg0, arg1, arg2)
-}
-
 // HandleCheckRunEvent mocks base method
 func (m *MockAPI) HandleCheckRunEvent(arg0 *github.CheckRunEvent) (bool, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "HandleCheckRunEvent", arg0)
 	ret0, _ := ret[0].(bool)
 	ret1, _ := ret[1].(error)
@@ -69,5 +45,34 @@ func (m *MockAPI) HandleCheckRunEvent(arg0 *github.CheckRunEvent) (bool, error) 
 
 // HandleCheckRunEvent indicates an expected call of HandleCheckRunEvent
 func (mr *MockAPIMockRecorder) HandleCheckRunEvent(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "HandleCheckRunEvent", reflect.TypeOf((*MockAPI)(nil).HandleCheckRunEvent), arg0)
+}
+
+// GetAzureArtifactsURL mocks base method
+func (m *MockAPI) GetAzureArtifactsURL(owner, repo string, buildID int64) string {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetAzureArtifactsURL", owner, repo, buildID)
+	ret0, _ := ret[0].(string)
+	return ret0
+}
+
+// GetAzureArtifactsURL indicates an expected call of GetAzureArtifactsURL
+func (mr *MockAPIMockRecorder) GetAzureArtifactsURL(owner, repo, buildID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAzureArtifactsURL", reflect.TypeOf((*MockAPI)(nil).GetAzureArtifactsURL), owner, repo, buildID)
+}
+
+// FetchAzureArtifact mocks base method
+func (m *MockAPI) FetchAzureArtifact(arg0 BuildArtifact, arg1 *multipart.Writer) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "FetchAzureArtifact", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// FetchAzureArtifact indicates an expected call of FetchAzureArtifact
+func (mr *MockAPIMockRecorder) FetchAzureArtifact(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FetchAzureArtifact", reflect.TypeOf((*MockAPI)(nil).FetchAzureArtifact), arg0, arg1)
 }

api/azure/webhook.go
@@ -128,7 +128,7 @@ func createAzureRun(
 	host := aeAPI.GetVersionedHostname()
 	writer.WriteField("callback_url", fmt.Sprintf("https://%s/api/results/create", host))
 
-	if err := azureAPI.FetchAzureArtifact(artifact, buf); err != nil {
+	if err := azureAPI.FetchAzureArtifact(artifact, writer); err != nil {
 		return err
 	}
 	if err := writer.Close(); err != nil {

api/receiver/receive_results.go
@@ -92,6 +92,8 @@ func HandleResultsUpload(a AppEngineAPI, w http.ResponseWriter, r *http.Request)
 
 	t, err := sendResultsToProcessor(a, uploader, results, getFile, extraParams)
 	if err != nil {
+		log := shared.GetLogger(a.Context())
+		log.Errorf("%s", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -143,10 +145,10 @@ func sendResultsToProcessor(
 
 	var errStr string
 	for err := range errors {
-		errStr += err.Error() + "\n"
+		errStr += err.Error()
 	}
 	if errStr != "" {
-		return nil, fmt.Errorf("error(s) occured when retrieving results from %s:\n%s", uploader, errStr)
+		return nil, fmt.Errorf("error(s) occured when transferring results from %s to GCS:\n%s", uploader, errStr)
 	}
 
 	return a.scheduleResultsTask(uploader, gcs, payloadType, extraParams)