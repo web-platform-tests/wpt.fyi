4b9fa8201e938da286805ce7eb9f2efb25a5755a
Luke Bjerring <lukebjerring@google.com>
2019-01-31 11:41:31 -0500

    Comment

--- api/query/concrete_query.go
+++ api/query/concrete_query.go
@@ -8,6 +8,8 @@ import (
 	"github.com/web-platform-tests/wpt.fyi/shared"
 )
 
+// AggregationOpts are options for the aggregation format used when collecting
+// the results.
 type AggregationOpts struct {
 	IncludeSubtests bool
 }
