web-platform-tests/wpt.fyi

commit ec9455cc5cf4f701663e16f2ddefde919092df64

Abstract Flag/Token related methods properly

webdriver/webapp_server.go
@@ -380,7 +380,7 @@ func addStaticData(i DevAppServerInstance) (err error) {
 	}
 
 	// Enable tested features
-	shared.SetFeature(ctx, shared.Flag{Name: "queryBuilder", Enabled: true})
+	datastore.Put(ctx, datastore.NewKey(ctx, "Flag", "queryBuilder", 0, nil), shared.Flag{Enabled: true})
 
 	return nil
 }

api/checks/jwt.go
@@ -79,7 +79,8 @@ func getJWTClient(ctx context.Context, appID, installation int64) (*http.Client,
 
 // https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-a-github-app
 func getSignedJWT(ctx context.Context, appID int64) (string, error) {
-	secret, err := shared.GetSecret(ctx, fmt.Sprintf("github-app-private-key-%v", appID))
+	ds := shared.NewAppEngineDatastore(ctx, false)
+	secret, err := shared.GetSecret(ds, fmt.Sprintf("github-app-private-key-%v", appID))
 	if err != nil {
 		return "", err
 	}

api/checks/webhook.go
@@ -28,6 +28,7 @@ func isWPTFYIApp(appID int64) bool {
 func checkWebhookHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := shared.NewAppEngineContext(r)
 	log := shared.GetLogger(ctx)
+	ds := shared.NewAppEngineDatastore(ctx, false)
 
 	contentType := r.Header.Get("Content-Type")
 	if contentType != "application/json" {
@@ -45,7 +46,7 @@ func checkWebhookHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	secret, err := shared.GetSecret(ctx, "github-check-webhook-secret")
+	secret, err := shared.GetSecret(ds, "github-check-webhook-secret")
 	if err != nil {
 		http.Error(w, "Unable to verify request: secret not found", http.StatusInternalServerError)
 		return

api/taskcluster/webhook.go
@@ -41,7 +41,8 @@ func tcWebhookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ctx := shared.NewAppEngineContext(r)
-	secret, err := shared.GetSecret(ctx, "github-tc-webhook-secret")
+	ds := shared.NewAppEngineDatastore(ctx, false)
+	secret, err := shared.GetSecret(ds, "github-tc-webhook-secret")
 	if err != nil {
 		http.Error(w, "Unable to verify request: secret not found", http.StatusInternalServerError)
 		return
@@ -116,12 +117,13 @@ func (b branchInfos) GetNames() []string {
 
 func handleStatusEvent(ctx context.Context, payload []byte) (bool, error) {
 	log := shared.GetLogger(ctx)
+	aeAPI := shared.NewAppEngineAPI(ctx)
 	var status statusEventPayload
 	if err := json.Unmarshal(payload, &status); err != nil {
 		return false, err
 	}
 
-	processAllBranches := shared.IsFeatureEnabled(ctx, flagTaskclusterAllBranches)
+	processAllBranches := aeAPI.IsFeatureEnabled(flagTaskclusterAllBranches)
 	if !shouldProcessStatus(log, processAllBranches, &status) {
 		return false, nil
 	}
@@ -164,7 +166,7 @@ func handleStatusEvent(ctx context.Context, payload []byte) (bool, error) {
 	checksAPI := checks.NewAPI(ctx)
 	err = createAllRuns(
 		log,
-		shared.NewAppEngineAPI(ctx),
+		aeAPI,
 		checksAPI,
 		*status.SHA,
 		username,

api/test_runs.go
@@ -5,7 +5,6 @@
 package api
 
 import (
-	"context"
 	"encoding/json"
 	"net/http"
 
@@ -23,6 +22,7 @@ const paginationTokenFeatureFlagName = "paginationTokens"
 func apiTestRunsHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := shared.NewAppEngineContext(r)
 	store := shared.NewAppEngineDatastore(ctx, true)
+	aeAPI := shared.NewAppEngineAPI(ctx)
 	q := r.URL.Query()
 	ids, err := shared.ParseRunIDsParam(q)
 	if err != nil {
@@ -50,15 +50,15 @@ func apiTestRunsHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		if pr != nil && shared.IsFeatureEnabled(ctx, "runsByPRNumber") {
-			filters.SHAs = getPRCommits(ctx, *pr)
+		if pr != nil && aeAPI.IsFeatureEnabled("runsByPRNumber") {
+			filters.SHAs = getPRCommits(aeAPI, *pr)
 		}
 		var runsByProduct shared.TestRunsByProduct
 		runsByProduct, err = LoadTestRunsForFilters(store, filters)
 
 		if err == nil {
 			testRuns = runsByProduct.AllRuns()
-			if shared.IsFeatureEnabled(ctx, paginationTokenFeatureFlagName) {
+			if aeAPI.IsFeatureEnabled(paginationTokenFeatureFlagName) {
 				nextPage := filters.NextPage(runsByProduct)
 				if nextPage != nil {
 					nextPageToken, _ = nextPage.Token()
@@ -133,15 +133,15 @@ func LoadTestRunsForFilters(store shared.Datastore, filters shared.TestRunFilter
 	return store.TestRunQuery().LoadTestRunsByKeys(keys)
 }
 
-func getPRCommits(ctx context.Context, pr int) shared.SHAs {
-	log := shared.GetLogger(ctx)
+func getPRCommits(aeAPI shared.AppEngineAPI, pr int) shared.SHAs {
+	log := shared.GetLogger(aeAPI.Context())
 
-	githubClient, err := shared.NewAppEngineAPI(ctx).GetGitHubClient()
+	githubClient, err := aeAPI.GetGitHubClient()
 	if err != nil {
 		log.Errorf("Failed to get github client: %s", err.Error())
 		return nil
 	}
-	commits, _, err := githubClient.PullRequests.ListCommits(ctx, "web-platform-tests", "wpt", pr, nil)
+	commits, _, err := githubClient.PullRequests.ListCommits(aeAPI.Context(), "web-platform-tests", "wpt", pr, nil)
 	if err != nil || commits == nil {
 		log.Errorf("Failed to fetch PR #%v: %s", pr, err.Error())
 		return nil

api/test_runs_medium_test.go
@@ -220,6 +220,7 @@ func TestGetTestRuns_Pagination(t *testing.T) {
 	assert.Nil(t, err)
 
 	ctx := shared.NewAppEngineContext(r)
+	ds := shared.NewAppEngineDatastore(ctx, false)
 	now := time.Now()
 	run := shared.TestRun{}
 	run.BrowserName = "chrome"
@@ -237,7 +238,7 @@ func TestGetTestRuns_Pagination(t *testing.T) {
 	assert.Equal(t, "", next)
 
 	// Feature enabled
-	shared.SetFeature(ctx, shared.Flag{Name: paginationTokenFeatureFlagName, Enabled: true})
+	shared.SetFeature(ds, shared.Flag{Name: paginationTokenFeatureFlagName, Enabled: true})
 	resp = httptest.NewRecorder()
 	apiTestRunsHandler(resp, r)
 	next = resp.Header().Get(nextPageTokenHeaderName)

shared/appengine.go
@@ -92,7 +92,8 @@ func (a AppEngineAPIImpl) GetSlowHTTPClient(timeout time.Duration) (*http.Client
 // GetGitHubClient returns a github client using the stored API token.
 func (a AppEngineAPIImpl) GetGitHubClient() (*github.Client, error) {
 	if a.githubClient == nil {
-		secret, err := GetSecret(a.ctx, "github-api-token")
+		ds := NewAppEngineDatastore(a.ctx, false)
+		secret, err := GetSecret(ds, "github-api-token")
 		if err != nil {
 			return nil, err
 		}
@@ -124,7 +125,8 @@ func (a AppEngineAPIImpl) IsAdmin() bool {
 // and its Enabled property is true.
 func (a AppEngineAPIImpl) IsFeatureEnabled(featureName string) bool {
 	// TODO(lukebjerring): Migrate other callers of this signature to AppEngineAPI
-	return IsFeatureEnabled(a.ctx, featureName)
+	ds := NewAppEngineDatastore(a.ctx, false)
+	return IsFeatureEnabled(ds, featureName)
 }
 
 // GetUploader returns the uploader with the given name.

shared/datastore.go
@@ -8,13 +8,12 @@ package shared
 
 import (
 	"context"
-
-	"google.golang.org/appengine/datastore"
 )
 
 // Key abstracts an int64 based datastore.Key
 type Key interface {
 	IntID() int64
+	StringID() string
 	Kind() string // Type name, e.g. TestRun
 }
 
@@ -52,9 +51,9 @@ type Datastore interface {
 }
 
 // GetFeatureFlags returns all feature flag defaults set in the datastore.
-func GetFeatureFlags(ctx context.Context) (flags []Flag, err error) {
-	var keys []*datastore.Key
-	keys, err = datastore.NewQuery("Flag").GetAll(ctx, &flags)
+func GetFeatureFlags(ds Datastore) (flags []Flag, err error) {
+	q := ds.NewQuery("Flag")
+	keys, err := ds.GetAll(q, &flags)
 	for i := range keys {
 		flags[i].Name = keys[i].StringID()
 	}
@@ -63,28 +62,28 @@ func GetFeatureFlags(ctx context.Context) (flags []Flag, err error) {
 
 // IsFeatureEnabled returns true if a feature with the given flag name exists,
 // and Enabled is set to true.
-func IsFeatureEnabled(ctx context.Context, flagName string) bool {
-	key := datastore.NewKey(ctx, "Flag", flagName, 0, nil)
+func IsFeatureEnabled(ds Datastore, flagName string) bool {
+	key := ds.NewNameKey("Flag", flagName)
 	flag := Flag{}
-	if err := datastore.Get(ctx, key, &flag); err != nil {
+	if err := ds.Get(key, &flag); err != nil {
 		return false
 	}
 	return flag.Enabled
 }
 
 // SetFeature puts a feature with the given flag name and enabled state.
-func SetFeature(ctx context.Context, flag Flag) error {
-	key := datastore.NewKey(ctx, "Flag", flag.Name, 0, nil)
-	_, err := datastore.Put(ctx, key, &flag)
+func SetFeature(ds Datastore, flag Flag) error {
+	key := ds.NewNameKey("Flag", flag.Name)
+	_, err := ds.Put(key, &flag)
 	return err
 }
 
 // GetSecret is a helper wrapper for loading a token's secret from the datastore
 // by name.
-func GetSecret(ctx context.Context, tokenName string) (string, error) {
-	key := datastore.NewKey(ctx, "Token", tokenName, 0, nil)
+func GetSecret(ds Datastore, tokenName string) (string, error) {
+	key := ds.NewNameKey("Token", tokenName)
 	var token Token
-	if err := datastore.Get(ctx, key, &token); err != nil {
+	if err := ds.Get(key, &token); err != nil {
 		return "", err
 	}
 	return token.Secret, nil

commit d78ec227c023349645325f011fb37c578aa5b07b

Fix static data setup in webdriver tests
Broken in ec9455cc5cf4f701663e16f2ddefde919092df64 (wrong type).

webdriver/webapp_server.go
@@ -380,7 +380,10 @@ func addStaticData(i DevAppServerInstance) (err error) {
 	}
 
 	// Enable tested features
-	datastore.Put(ctx, datastore.NewKey(ctx, "Flag", "queryBuilder", 0, nil), shared.Flag{Enabled: true})
+	_, err = datastore.Put(ctx, datastore.NewKey(ctx, "Flag", "queryBuilder", 0, nil), &shared.Flag{Enabled: true})
+	if err != nil {
+		panic(err)
+	}
 
 	return nil
 }