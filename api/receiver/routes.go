e6f0f04f8b13307b81518e72bb6157bf964372f1
Robert Ma <robertma@chromium.org>
2019-03-12 15:12:50 -0400

    Move handlers & routes for results receiver
    into the receiver package for consistency.

---
+++ api/receiver/routes.go
@@ -0,0 +1,18 @@
+// Copyright 2019 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package receiver
+
+import "github.com/web-platform-tests/wpt.fyi/shared"
+
+// RegisterRoutes adds all the result receiver route handlers.
+func RegisterRoutes() {
+	// PROTECTED API endpoint for receiving test results (wptreport) from runners.
+	// This API is authenticated. Runners have credentials.
+	shared.AddRoute("/api/results/upload", "api-results-upload", apiResultsUploadHandler)
+
+	// PRIVATE API endpoint for creating a test run in Datastore.
+	// This API is authenticated. Only this AppEngine project has the credential.
+	shared.AddRoute("/api/results/create", "api-results-create", apiResultsCreateHandler)
+}
