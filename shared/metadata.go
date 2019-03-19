38a671bf58bd68aa5fcd07bc72bc0a2dfd9ad9de
kyle Ju <kyleju@chromium.org>
2019-03-18 20:47:00 -0400

    parse metadata from wpt-metadata repo

--- shared/metadata.go
+++ shared/metadata.go
@@ -4,6 +4,11 @@
 
 package shared
 
+import (
+	"github.com/go-yaml/yaml"
+	"github.com/web-platform-tests/wpt-metadata/util"
+)
+
 // Metadata represents a wpt-metadata META.yml file.
 type Metadata struct {
 	Links MetadataLinks
@@ -19,4 +24,25 @@ type MetadataLink struct {
 	Product  ProductSpec
 	TestPath string `yaml:"test"`
 	URL      string
+}
+
+// RetrieveMetadata collects and parses all META.yml files from
+// wpt-metadata reposiroty.
+func RetrieveMetadata() map[string]Metadata {
+	return parseMetadata(util.CollectMetadata())
+}
+
+// parseMetadata implements the parsing logic.
+func parseMetadata(metadataByteMap map[string][]byte) map[string]Metadata {
+	var metadataMap = make(map[string]Metadata)
+
+	for path, data := range metadataByteMap {
+		var metadata Metadata
+		err := yaml.Unmarshal(data, &metadata)
+		if err != nil {
+			panic(err)
+		}
+		metadataMap[path] = metadata
+	}
+	return metadataMap
 }
