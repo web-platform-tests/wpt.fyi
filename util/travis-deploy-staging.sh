bdc99d2e02351f5d3d64b4d27ef7fa301756f0a7
Robert Ma <bob1211@gmail.com>
2019-04-23 12:53:37 -0400

    Deploy staging searchcache less often (#1279)

--- util/travis-deploy-staging.sh
+++ util/travis-deploy-staging.sh
@@ -23,7 +23,8 @@ if [[ "${APP_PATH}" == ""  ]]; then fatal "app path not specified."; fi
 APP_DEPS="${APP_PATH}"
 if [[ "${APP_PATH}" == "webapp" ]]; then APP_DEPS="${APP_DEPS}|api|shared"; fi
 if [[ "${APP_PATH}" == "revisions/service" ]]; then APP_DEPS="${APP_DEPS}|revisions|shared"; fi
-if [[ "${APP_PATH}" == "api/query/cache/service" ]]; then APP_DEPS="shared|api/query"; fi
+# Be more conservative: only deploy searchcache when it's directly modified.
+# if [[ "${APP_PATH}" == "api/query/cache/service" ]]; then APP_DEPS="shared|api/query"; fi
 APP_DEPS_REGEX="^(${APP_DEPS})/"
 
 EXCLUSIONS="_test.go$|webapp/components/test/"
