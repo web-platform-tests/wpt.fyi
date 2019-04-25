39bb9e2c664362c9da643ed08871aed3e0b81881
Luke Bjerring <lukebjerring@google.com>
2019-04-25 14:01:48 -0400

    Bugfix

--- api/query/cache/service/main.go
+++ api/query/cache/service/main.go
@@ -219,7 +219,7 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 		var netClient = &http.Client{
 			Timeout: time.Second * 5,
 		}
-		resp.MetadataResponse = shared.GetMetadataResponse(runs, netClient, log.StandardLogger())
+		resp.MetadataResponse = shared.GetMetadataResponse(runs, netClient, logrus.StandardLogger())
 	}
 
 	data, err = json.Marshal(resp)
