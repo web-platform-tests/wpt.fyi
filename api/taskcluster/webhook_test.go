4231b40567b1c8f094163a15619cd6dde9f1402e
Robert Ma <robertma@chromium.org>
2019-10-08 18:26:14 -0400

    Update go deps (#1529)
    * Update go deps
    
    * `go get -u all` mainly to get taskcluster/client-go v19
    * Fixed the expected Go version to match GAE runtime
    * Removed unnecessary redirects
    * `go mod tidy` to remove unused requires
    
    * Fix gocheck
    
    * fixup
    
    * comment

--- api/taskcluster/webhook_test.go
+++ api/taskcluster/webhook_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/google/go-github/v28/github"
 	"github.com/stretchr/testify/assert"
-	"github.com/taskcluster/taskcluster/clients/client-go/v18/tcqueue"
+	"github.com/taskcluster/taskcluster/clients/client-go/v19/tcqueue"
 	uc "github.com/web-platform-tests/wpt.fyi/api/receiver/client"
 	"github.com/web-platform-tests/wpt.fyi/shared"
 	"github.com/web-platform-tests/wpt.fyi/shared/sharedtest"
