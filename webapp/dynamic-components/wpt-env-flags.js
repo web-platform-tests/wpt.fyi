30e125bee61223c38796bc99ff1362fd47824061
Luke Bjerring <lukebjerring@google.com>
2019-10-09 17:06:36 -0400

    Fix wpt-env-flags.js

--- webapp/dynamic-components/wpt-env-flags.js
+++ webapp/dynamic-components/wpt-env-flags.js
@@ -1,24 +1,5 @@
 /**
- * Copyright 2018 The WPT Dashboard Project. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file.
+ * This is an empty stub for dynamic-components/wpt-env-flags.js used in tests.
  */
-
-/*
-WPTEnvironmentFlags is a class containing default enviroment wpt.fyi
-feature flags.
-*/
-const WPTEnvironmentFlags = class WPTEnvironmentFlags {}
-{{range .Flags}}
-Object.defineProperty(
-  WPTEnvironmentFlags,
-  '{{.Name}}',
-  {
-    writable: false,
-    configurable: false,
-    value: {{.Enabled}},
-  }
-);
-{{end}}
-
+const WPTEnvironmentFlags = undefined;
 export { WPTEnvironmentFlags };
