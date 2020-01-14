43dbd1da1d998fdb7d05cf0904b574cd4823f68e
kyle Ju <kyleju@chromium.org>
2020-01-14 13:58:32 -0500

    add license

--- shared/metadata_util_test.go
+++ shared/metadata_util_test.go
@@ -1,3 +1,9 @@
+// +build small
+
+// Copyright 2020 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package shared
 
 import (
