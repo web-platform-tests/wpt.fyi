c864399b3ddb630b84e80ad2a29c74d91d03ba2b
Robert Ma <robertma@chromium.org>
2020-02-26 17:32:10 -0500

    Support MANIFEST v8 (trie)
    FilterByPath is now reimplemented with on-demand unmarshalling which
    takes the advantage of the new Trie structure.
    
    Move the related code from models.go into manifest.go and add more
    tests.
    
    Fixes #1777.

--- shared/manifest_test.go
+++ shared/manifest_test.go
@@ -7,10 +7,93 @@
 package shared
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestManifestFilterByPath(t *testing.T) {
+	manifest := []byte(`{
+"items": {
+	"testharness": {
+		"foo": {
+			"bar": {
+				"test.html": [
+					"1d3166465cc6f2e8f9f18f53e499ca61e12d59bd",
+					[null, {}]
+				]
+
+			}
+		},
+		"foobar": {
+			"mytest.html": [
+				"2d3166465cc6f2e8f9f18f53e499ca61e12d59bd",
+				[null, {}]
+			]
+		}
+	},
+	"manual": {
+		"foobar": {
+			"test-manual.html": [
+				"3d3166465cc6f2e8f9f18f53e499ca61e12d59bd",
+				[null, {}]
+			]
+		}
+	}
+},
+"version": 8
+}`)
+
+	t.Run("empty match", func(t *testing.T) {
+		var m Manifest
+		err := json.Unmarshal(manifest, &m)
+		assert.Nil(t, err)
+
+		res, err := m.FilterByPath("/non-existent")
+		assert.Nil(t, err)
+		assert.Equal(t, 0, len(res.Items))
+		assert.Equal(t, 8, res.Version)
+	})
+
+	t.Run("match nested", func(t *testing.T) {
+		var m Manifest
+		err := json.Unmarshal(manifest, &m)
+		assert.Nil(t, err)
+
+		res, err := m.FilterByPath("/foo/bar")
+		assert.Nil(t, err)
+		assert.Equal(t, `{"foo":{"bar":{"test.html":["1d3166465cc6f2e8f9f18f53e499ca61e12d59bd",[null,{}]]}}}`, string(res.Items["testharness"]))
+		_, ok := res.Items["manual"]
+		assert.False(t, ok)
+		assert.Equal(t, 8, res.Version)
+	})
+
+	t.Run("match single", func(t *testing.T) {
+		var m Manifest
+		err := json.Unmarshal(manifest, &m)
+		assert.Nil(t, err)
+
+		res, err := m.FilterByPath("/foo")
+		assert.Nil(t, err)
+		assert.Equal(t, `{"foo":{"bar":{"test.html":["1d3166465cc6f2e8f9f18f53e499ca61e12d59bd",[null,{}]]}}}`, string(res.Items["testharness"]))
+		_, ok := res.Items["manual"]
+		assert.False(t, ok)
+		assert.Equal(t, 8, res.Version)
+	})
+
+	t.Run("match multiple", func(t *testing.T) {
+		var m Manifest
+		err := json.Unmarshal(manifest, &m)
+		assert.Nil(t, err)
+
+		res, err := m.FilterByPath("/foobar")
+		assert.Nil(t, err)
+		assert.Equal(t, `{"foobar":{"mytest.html":["2d3166465cc6f2e8f9f18f53e499ca61e12d59bd",[null,{}]]}}`, string(res.Items["testharness"]))
+		assert.Equal(t, `{"foobar":{"test-manual.html":["3d3166465cc6f2e8f9f18f53e499ca61e12d59bd",[null,{}]]}}`, string(res.Items["manual"]))
+		assert.Equal(t, 8, res.Version)
+	})
+}
 
 func TestExplodePossibleFilename_AnyJS(t *testing.T) {
 	filename := "/test/file.any.js"
