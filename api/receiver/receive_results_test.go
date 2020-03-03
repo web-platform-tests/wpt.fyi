web-platform-tests/wpt.fyi

commit 9831b916d1edbf5baab7e021398867a554b93ac4

Migrate from appengine/urlfetch to net/http (#1811)
* Migrate from appengine/urlfetch to net/http

We still don't know why #1808 is happening, but based on experiments
switching to net/http fixes #1808, and it is recommended anyway (also
part of #1747).

This unfortunately changes the signature of a widely used method.

* Fix TLS cert errors

appengine.ModuleHostname returns a dotted URL (version.service) that
fails to match the certificate on *.appspot.com (not a double wildcard).
Always construct the "version-dot-service" form ourselves.

This didn't cause any trouble presumably because of some magic in
appengine.urlfetch.

api/receiver/client/client.go
@@ -77,9 +77,8 @@ func (c client) CreateRun(
 	}
 	req.SetBasicAuth(username, password)
 
-	slowClient, cancel := c.aeAPI.GetSlowHTTPClient(UploadTimeout)
-	defer cancel()
-	resp, err := slowClient.Do(req)
+	hc := c.aeAPI.GetHTTPClientWithTimeout(UploadTimeout)
+	resp, err := hc.Do(req)
 	if err != nil {
 		return err
 	}

api/azure/api_test.go
@@ -98,7 +98,7 @@ func TestHandleCheckRunEvent(t *testing.T) {
 	aeAPI.EXPECT().GetResultsUploadURL().Return(uploadURL)
 	aeAPI.EXPECT().GetUploader("azure").Return(shared.Uploader{Username: "azure", Password: "123"}, nil)
 	aeAPI.EXPECT().GetHTTPClient().AnyTimes().Return(server.Client())
-	aeAPI.EXPECT().GetSlowHTTPClient(gomock.Any()).AnyTimes().Return(server.Client(), func() {})
+	aeAPI.EXPECT().GetHTTPClientWithTimeout(gomock.Any()).AnyTimes().Return(server.Client())
 
 	log, hook := logrustest.NewNullLogger()
 	ctx := context.WithValue(context.Background(), shared.DefaultLoggerCtxKey(), log)

api/query/search.go
@@ -111,8 +111,7 @@ func (sh structuredSearchHandler) ServeHTTP(w http.ResponseWriter, r *http.Reque
 		logger := shared.GetLogger(ctx)
 		logger.Infof("Forwarding structured search request to %s: %s", hostname, string(data))
 
-		client, cancel := sh.api.GetSlowHTTPClient(time.Second * 15)
-		defer cancel()
+		client := sh.api.GetHTTPClientWithTimeout(time.Second * 15)
 		req, err := http.NewRequest("POST", fwdURL.String(), bytes.NewBuffer(data))
 		if err != nil {
 			logger.Errorf("Failed to create request to POST %s: %v", fwdURL.String(), err)

api/query/search_test.go
@@ -133,7 +133,7 @@ func TestStructuredSearchHandler_success(t *testing.T) {
 
 	api.EXPECT().Context().Return(sharedtest.NewTestContext())
 	api.EXPECT().GetServiceHostname("searchcache").Return(hostname)
-	api.EXPECT().GetSlowHTTPClient(gomock.Any()).Return(server.Client(), func() {})
+	api.EXPECT().GetHTTPClientWithTimeout(gomock.Any()).Return(server.Client())
 	w := httptest.NewRecorder()
 	structuredSearchHandler{queryHandler{}, api}.ServeHTTP(w, r)
 
@@ -163,7 +163,7 @@ func TestStructuredSearchHandler_failure(t *testing.T) {
 
 	api.EXPECT().Context().Return(sharedtest.NewTestContext())
 	api.EXPECT().GetServiceHostname("searchcache").Return(hostname)
-	api.EXPECT().GetSlowHTTPClient(gomock.Any()).Return(server.Client(), func() {})
+	api.EXPECT().GetHTTPClientWithTimeout(gomock.Any()).Return(server.Client())
 
 	w := httptest.NewRecorder()
 	structuredSearchHandler{queryHandler{}, api}.ServeHTTP(w, r)

api/receiver/mock_receiver/api_mock.go
@@ -98,6 +98,20 @@ func (mr *MockAPIMockRecorder) GetHTTPClient() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetHTTPClient", reflect.TypeOf((*MockAPI)(nil).GetHTTPClient))
 }
 
+// GetHTTPClientWithTimeout mocks base method
+func (m *MockAPI) GetHTTPClientWithTimeout(arg0 time.Duration) *http.Client {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetHTTPClientWithTimeout", arg0)
+	ret0, _ := ret[0].(*http.Client)
+	return ret0
+}
+
+// GetHTTPClientWithTimeout indicates an expected call of GetHTTPClientWithTimeout
+func (mr *MockAPIMockRecorder) GetHTTPClientWithTimeout(arg0 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetHTTPClientWithTimeout", reflect.TypeOf((*MockAPI)(nil).GetHTTPClientWithTimeout), arg0)
+}
+
 // GetHostname mocks base method
 func (m *MockAPI) GetHostname() string {
 	m.ctrl.T.Helper()
@@ -166,21 +180,6 @@ func (m *MockAPI) GetServiceHostname(arg0 string) string {
 func (mr *MockAPIMockRecorder) GetServiceHostname(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetServiceHostname", reflect.TypeOf((*MockAPI)(nil).GetServiceHostname), arg0)
-}
-
-// GetSlowHTTPClient mocks base method
-func (m *MockAPI) GetSlowHTTPClient(arg0 time.Duration) (*http.Client, context.CancelFunc) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GetSlowHTTPClient", arg0)
-	ret0, _ := ret[0].(*http.Client)
-	ret1, _ := ret[1].(context.CancelFunc)
-	return ret0, ret1
-}
-
-// GetSlowHTTPClient indicates an expected call of GetSlowHTTPClient
-func (mr *MockAPIMockRecorder) GetSlowHTTPClient(arg0 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetSlowHTTPClient", reflect.TypeOf((*MockAPI)(nil).GetSlowHTTPClient), arg0)
 }
 
 // GetUploader mocks base method

api/taskcluster/webhook_test.go
@@ -256,7 +256,7 @@ func TestCreateAllRuns_success(t *testing.T) {
 	defer mockC.Finish()
 	aeAPI := sharedtest.NewMockAppEngineAPI(mockC)
 	aeAPI.EXPECT().GetVersionedHostname().AnyTimes().Return("localhost:8080")
-	aeAPI.EXPECT().GetSlowHTTPClient(uc.UploadTimeout).AnyTimes().Return(&http.Client{}, func() {})
+	aeAPI.EXPECT().GetHTTPClientWithTimeout(uc.UploadTimeout).AnyTimes().Return(server.Client())
 	aeAPI.EXPECT().GetResultsUploadURL().AnyTimes().Return(serverURL)
 
 	t.Run("master", func(t *testing.T) {
@@ -319,7 +319,7 @@ func TestCreateAllRuns_one_error(t *testing.T) {
 
 	aeAPI := sharedtest.NewMockAppEngineAPI(mockC)
 	aeAPI.EXPECT().GetVersionedHostname().MinTimes(1).Return("localhost:8080")
-	aeAPI.EXPECT().GetSlowHTTPClient(uc.UploadTimeout).Times(2).Return(&http.Client{}, func() {})
+	aeAPI.EXPECT().GetHTTPClientWithTimeout(uc.UploadTimeout).Times(2).Return(server.Client())
 	serverURL, _ := url.Parse(server.URL)
 	aeAPI.EXPECT().GetResultsUploadURL().AnyTimes().Return(serverURL)
 
@@ -355,7 +355,7 @@ func TestCreateAllRuns_all_errors(t *testing.T) {
 	aeAPI := sharedtest.NewMockAppEngineAPI(mockC)
 	aeAPI.EXPECT().GetVersionedHostname().MinTimes(1).Return("localhost:8080")
 	// Give a very short timeout (instead of the asked 1min) to make tests faster.
-	aeAPI.EXPECT().GetSlowHTTPClient(uc.UploadTimeout).MinTimes(1).Return(&http.Client{Timeout: time.Microsecond}, func() {})
+	aeAPI.EXPECT().GetHTTPClientWithTimeout(uc.UploadTimeout).MinTimes(1).Return(&http.Client{Timeout: time.Microsecond})
 	serverURL, _ := url.Parse(server.URL)
 	aeAPI.EXPECT().GetResultsUploadURL().AnyTimes().Return(serverURL)
 

shared/appengine.go
@@ -18,7 +18,6 @@ import (
 	"github.com/google/go-github/v28/github"
 	"golang.org/x/oauth2"
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/urlfetch"
 	"google.golang.org/appengine/user"
 )
 
@@ -28,9 +27,9 @@ type AppEngineAPI interface {
 
 	// GitHub OAuth client
 	GetGitHubClient() (*github.Client, error)
-	// urlfetch cilents
+	// http.Client
 	GetHTTPClient() *http.Client
-	GetSlowHTTPClient(time.Duration) (*http.Client, context.CancelFunc)
+	GetHTTPClientWithTimeout(time.Duration) *http.Client
 
 	// AppEngine User API
 	IsAdmin() bool
@@ -39,26 +38,32 @@ type AppEngineAPI interface {
 
 	// GetVersion returns the version name for the current environment.
 	GetVersion() string
-	// GetHostname returns the canonical hostname for the current AppEngine project,
-	// i.e. staging.wpt.fyi or wpt.fyi.
+	// GetHostname returns the canonical hostname for the current AppEngine
+	// project, i.e. staging.wpt.fyi or wpt.fyi.
 	GetHostname() string
-	// GetVersionedHostname returns the AppEngine hostname for the current version,
-	// i.e. version-dot-wptdashboard{,-staging}.appspot.com.
+	// GetVersionedHostname returns the AppEngine hostname for the current
+	// version of the default service, i.e.,
+	//   version-dot-wptdashboard{,-staging}.appspot.com.
+	// Note: if the default service does not have the current version,
+	// AppEngine routing will find a version according to traffic split.
+	// https://cloud.google.com/appengine/docs/standard/go/how-requests-are-routed#soft_routing
 	GetVersionedHostname() string
-	// GetServiceHostname returns the AppEngine hostname for the given service (module) of the
-	// current version,
-	// i.e. version-dot-service-dot-wptdashboard{,-staging}.appspot.com.
-	// Note that if the current version does not have the specified service, AppEngine routing
-	// will fall back to its default version.
+	// GetServiceHostname returns the AppEngine hostname for the current
+	// version of the given service, i.e.,
+	//   version-dot-service-dot-wptdashboard{,-staging}.appspot.com.
+	// Note: if the specified service does not have the current version,
+	// AppEngine routing will find a version according to traffic split;
+	// if the service does not exist at all, AppEngine will fall back to
+	// the default service.
 	GetServiceHostname(service string) string
 
-	// GetResultsURL returns a URL for the wpt.fyi results page for the test runs loaded for the
+	// GetResultsURL returns a URL to {staging.,}wpt.fyi/results with the
 	// given filter.
 	GetResultsURL(filter TestRunFilter) *url.URL
-	// GetRunsURL returns a URL for the wpt.fyi results page for the test runs loaded for the
-	// given filter.
+	// GetRunsURL returns a URL to {staging.,}wpt.fyi/runs with the given
+	// filter.
 	GetRunsURL(filter TestRunFilter) *url.URL
-	// GetResultsUploadURL returns a URL for uploading results (i.e. results receiver).
+	// GetResultsUploadURL returns a URL for uploading results.
 	GetResultsUploadURL() *url.URL
 
 	// Simple wrappers that delegate to Datastore
@@ -76,9 +81,7 @@ func NewAppEngineAPI(ctx context.Context) AppEngineAPI {
 
 // appEngineAPIImpl implements the AppEngineAPI interface.
 type appEngineAPIImpl struct {
-	ctx context.Context
-	// Cached client objects
-	httpClient   *http.Client
+	ctx          context.Context
 	githubClient *github.Client
 }
 
@@ -86,16 +89,13 @@ func (a appEngineAPIImpl) Context() context.Context {
 	return a.ctx
 }
 
-func (a *appEngineAPIImpl) GetHTTPClient() *http.Client {
-	if a.httpClient == nil {
-		a.httpClient = urlfetch.Client(a.ctx)
-	}
-	return a.httpClient
+func (a appEngineAPIImpl) GetHTTPClient() *http.Client {
+	// Set timeout to 5s for compatibility with legacy appengine.urlfetch.Client.
+	return a.GetHTTPClientWithTimeout(time.Second * 5)
 }
 
-func (a appEngineAPIImpl) GetSlowHTTPClient(timeout time.Duration) (*http.Client, context.CancelFunc) {
-	slowCtx, cancel := context.WithTimeout(a.ctx, timeout)
-	return urlfetch.Client(slowCtx), cancel
+func (a appEngineAPIImpl) GetHTTPClientWithTimeout(timeout time.Duration) *http.Client {
+	return &http.Client{Timeout: timeout}
 }
 
 func (a *appEngineAPIImpl) GetGitHubClient() (*github.Client, error) {
@@ -161,15 +161,7 @@ func (a appEngineAPIImpl) GetVersionedHostname() string {
 }
 
 func (a appEngineAPIImpl) GetServiceHostname(service string) string {
-	// version and instance (last 2 params) left blank means that the version of the current
-	// instance will be used. This is desirable for branches that push multiple services, and
-	// means that we should keep service production versions in sync.
-	hostname, err := appengine.ModuleHostname(a.ctx, service, "", "")
-	if err == nil {
-		return hostname
-	}
-	// Fallback to roughly the same strategy.
-	hostname = appengine.DefaultVersionHostname(a.ctx)
+	hostname := appengine.DefaultVersionHostname(a.ctx)
 	return fmt.Sprintf("%s-dot-%s-dot-%s", a.GetVersion(), service, hostname)
 }
 

shared/run_diff.go
@@ -390,7 +390,7 @@ func (d diffAPIImpl) getRunsDiffFromSearchCache(before, after TestRun, filter Di
 	}
 	body, _ := json.Marshal(diffBody{RunIDs: []int64{before.ID, after.ID}})
 
-	client, _ := d.aeAPI.GetSlowHTTPClient(time.Second * 10)
+	client := d.aeAPI.GetHTTPClientWithTimeout(time.Second * 10)
 	resp, err := client.Post(diffURL.String(), "application/json", bytes.NewBuffer(body))
 	if err != nil {
 		return diff, err

commit 34539c81400e6b5f420edbcabe5dd25f97d1be46

Add the missing Content-Type header back
This attempts to fix #1830.

Missing Content-Type: application/x-url-form-encoded when posting an
HTTP form is clearly a bug, but it is unclear why it only started to
surface after #1811.

A preventative measure is also added to return an error when the upload
payload is empty, along with some minor improvements to logging.

api/receiver/client/client.go
@@ -76,6 +76,7 @@ func (c client) CreateRun(
 		return err
 	}
 	req.SetBasicAuth(username, password)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	hc := c.aeAPI.GetHTTPClientWithTimeout(UploadTimeout)
 	resp, err := hc.Do(req)

api/receiver/receive_results.go
@@ -62,15 +62,16 @@ func HandleResultsUpload(a API, w http.ResponseWriter, r *http.Request) {
 
 	log := shared.GetLogger(a.Context())
 	var results, screenshots []string
-	if r.MultipartForm != nil && r.MultipartForm.File != nil && len(r.MultipartForm.File["result_file"]) > 0 {
+	if f := r.MultipartForm; f != nil && f.File != nil && len(f.File["result_file"]) > 0 {
 		// result_file[] payload
-		files := r.MultipartForm.File["result_file"]
-		sFiles := r.MultipartForm.File["screenshot_file"]
+		files := f.File["result_file"]
+		sFiles := f.File["screenshot_file"]
 		log.Debugf("Found %d result files, %d screenshot files", results, screenshots)
 		var err error
 		results, screenshots, err = saveToGCS(a, uploader, files, sFiles)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Errorf("Failed to save files to GCS: %v", err)
+			http.Error(w, "Failed to save files to GCS", http.StatusInternalServerError)
 			return
 		}
 	} else if artifactName := getAzureArtifactName(r.PostForm.Get("result_url")); artifactName != "" {
@@ -78,17 +79,21 @@ func HandleResultsUpload(a API, w http.ResponseWriter, r *http.Request) {
 		azureURL := r.PostForm.Get("result_url")
 		log.Debugf("Found Azure URL: %s", azureURL)
 		extraParams["azure_url"] = azureURL
-	} else {
+	} else if len(r.PostForm["result_url"]) > 0 {
 		// General result_url[] payload
 		results = r.PostForm["result_url"]
 		screenshots = r.PostForm["screenshot_url"]
 		log.Debugf("Found %d result URLs, %d screenshot URLs", results, screenshots)
+	} else {
+		log.Errorf("No results found")
+		http.Error(w, "No results found", http.StatusBadRequest)
+		return
 	}
 
 	t, err := a.ScheduleResultsTask(uploader, results, screenshots, extraParams)
 	if err != nil {
-		log.Errorf("%s", err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Errorf("Failed to schedule task: %v", err)
+		http.Error(w, "Failed to schedule task", http.StatusInternalServerError)
 		return
 	}
 	log.Infof("Task %s added to queue", t.Name)

api/receiver/receive_results_test.go
@@ -260,3 +260,23 @@ func TestHandleResultsUpload_fail_uploading(t *testing.T) {
 	HandleResultsUpload(mockAE, resp, req)
 	assert.Equal(t, resp.Code, http.StatusInternalServerError)
 }
+
+func TestHandleResultsUpload_empty_payload(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	req := httptest.NewRequest("POST", "/api/results/upload", nil)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth("blade-runner", "123")
+	resp := httptest.NewRecorder()
+
+	mockAE := mock_receiver.NewMockAPI(mockCtrl)
+	mockAE.EXPECT().Context().Return(sharedtest.NewTestContext()).AnyTimes()
+	gomock.InOrder(
+		mockAE.EXPECT().IsAdmin().Return(false),
+		mockAE.EXPECT().GetUploader("blade-runner").Return(shared.Uploader{"blade-runner", "123"}, nil),
+	)
+
+	HandleResultsUpload(mockAE, resp, req)
+	assert.Equal(t, resp.Code, http.StatusBadRequest)
+}