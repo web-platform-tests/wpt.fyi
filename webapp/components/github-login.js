38fb02dd11c649fd2e020ea2a0bc27215a140197
Kyle Ju <kyleju@chromium.org>
2020-04-06 16:28:00 -0400

    Place triage mode toggle next to sign in

--- webapp/components/github-login.js
+++ webapp/components/github-login.js
@@ -32,29 +32,32 @@ class GitHubLogin extends WPTFlags(PolymerElement) {
       .help {
         vertical-align: baseline;
       }
+      .triage-toggle {
+        display: inline;
+      }
     </style>
-    <iron-icon class="help" src="/static/help.svg" onclick="[[openHelpDialog]]"></iron-icon>
     <template is="dom-if" if="[[!user]]">
+     <iron-icon class="help" src="/static/help.svg" onclick="[[openHelpDialog]]"></iron-icon>
       <paper-button class="log-in-button" raised onclick="[[logIn]]">
       <iron-icon class="github-icon" src="/static/github.svg"></iron-icon>
       Sign in with GitHub
       </paper-button>
     </template>
     <template is="dom-if" if="[[user]]">
-      <iron-icon class="github-icon" src="/static/github.svg"></iron-icon>
-      [[user]]
-      <paper-icon-button title="Sign out" icon="exit-to-app" onclick="[[logOut]]"></paper-icon-button>
+      <div>
+        <paper-toggle-button class="triage-toggle" checked="{{multiselectTriageUI}}">
+          Triage Mode
+         </paper-toggle-button>
+        <iron-icon class="github-icon" src="/static/github.svg"></iron-icon>
+          [[user]]
+        <paper-icon-button title="Sign out" icon="exit-to-app" onclick="[[logOut]]"></paper-icon-button>
+      </div>
     </template>
     <paper-dialog id="dialog">
       <h3>wpt.fyi Login</h3>
       <div>Logging in to wpt.fyi enables users to have a customized landing page, set default
       configurations, and triage tests from the wpt.fyi UI </div>
       <div>To annotate tests, click on Triage Mode below </div>
-      <template is="dom-if" if="[[user]]">
-        <paper-toggle-button checked="{{multiselectTriageUI}}">
-          Triage Mode
-        </paper-toggle-button>
-      </template>
       <div class="buttons">
         <paper-button dialog-dismiss>Dismiss</paper-button>
       </div>
@@ -71,7 +74,7 @@ class GitHubLogin extends WPTFlags(PolymerElement) {
       user: {
         type: String,
         value: null,
-      }
+      },
     };
   }
 
