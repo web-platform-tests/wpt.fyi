ffc77ef22d6fe665b8173a1563edb6cfa72088e4
Luke Zielinski <lpz@chromium.org>
2020-04-16 10:26:09 -0400

    A few methods now use arrays of pointers, update the code to handle that.

--- go.mod
+++ go.mod
@@ -15,7 +15,6 @@ require (
 	github.com/gobuffalo/packr/v2 v2.8.0
 	github.com/golang/mock v1.4.3
 	github.com/golang/protobuf v1.4.0 // indirect
-	github.com/google/go-github/v29 v29.0.3
 	github.com/google/go-github/v31 v31.0.0
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/handlers v1.4.2
--- shared/triage_metadata.go
+++ shared/triage_metadata.go
@@ -108,10 +108,11 @@ func (tm triageMetadata) getCommitBranchRef() (ref *github.Reference, err error)
 func (tm triageMetadata) getTree(ref *github.Reference, triagedMetadataMap map[string][]byte) (tree *github.Tree, err error) {
 	client := tm.githubClient
 
-	entries := []github.TreeEntry{}
+	entries := []*github.TreeEntry{}
 	for folderPath, content := range triagedMetadataMap {
 		dest := GetMetadataFilePath(folderPath)
-		entries = append(entries, github.TreeEntry{Path: github.String(dest), Type: github.String("blob"), Content: github.String(string(content)), Mode: github.String("100644")})
+		newEntry := github.TreeEntry{Path: github.String(dest), Type: github.String("blob"), Content: github.String(string(content)), Mode: github.String("100644")}
+		entries = append(entries, &newEntry)
 	}
 
 	tree, _, err = client.Git.CreateTree(tm.ctx, tm.sourceOwner, tm.sourceRepo, *ref.Object.SHA, entries)
@@ -131,7 +132,7 @@ func (tm triageMetadata) pushCommit(ref *github.Reference, tree *github.Tree) (e
 	// Create the commit using the tree.
 	date := time.Now()
 	author := &github.CommitAuthor{Date: &date, Name: &tm.authorName, Email: &tm.authorEmail}
-	commit := &github.Commit{Author: author, Message: &tm.commitMessage, Tree: tree, Parents: []github.Commit{*parent.Commit}}
+	commit := &github.Commit{Author: author, Message: &tm.commitMessage, Tree: tree, Parents: []*github.Commit{parent.Commit}}
 	newCommit, _, err := client.Git.CreateCommit(tm.ctx, tm.sourceOwner, tm.sourceRepo, commit)
 	if err != nil {
 		return err
