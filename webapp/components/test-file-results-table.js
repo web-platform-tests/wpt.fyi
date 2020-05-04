c6e2c45f9b8561988cf8a7d1c95cbffac8783513
Kyle Ju <kyleju@chromium.org>
2020-05-04 09:10:47 -0400

    wip3

--- webapp/components/test-file-results-table.js
+++ webapp/components/test-file-results-table.js
@@ -286,7 +286,7 @@ class TestFileResultsTable extends WPTFlags(Pluralizer(WPTColors(PathInfo(TestRu
   }
 
   isTriageModeUpdated(isTriageMode) {
-    this.row = Object.value(this.row);
+    this.rows = Object.value(this.rows);
   }
 
   subtestMessage(result, verbose) {
