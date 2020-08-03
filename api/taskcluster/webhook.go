926462fe9cfe0000785018da2ae6595051a035eb
Stephen McGruer <smcgruer@chromium.org>
2020-08-03 15:24:15 -0400

    [Taskcluster] Reject tasks that have non-success state
    During the migration to the GitHub Checks API for Taskcluster, we
    mistakenly interpreted the 'status' field of a CheckRun as equivalent to
    the 'status' field of a Taskcluster task. This is not the case; in the
    Taskcluster case 'status' captures the success/failure state of a task,
    but for CheckRuns it merely captures the finished/not-finished state.
    
    Instead, CheckRuns should use the 'conclusion' field to get the result
    of the task.
    
    To keep things compatible with the existing logic that assumes
    Taskcluster status results, we map 'success' --> 'completed'. In the
    future we should remove the old status code and switch to just using the
    conclusion.
    
    Issue: https://github.com/web-platform-tests/wpt.fyi/issues/2091

--- api/taskcluster/webhook.go
+++ api/taskcluster/webhook.go
@@ -163,12 +163,18 @@ func GetCheckSuiteEventInfo(checkSuite github.CheckSuiteEvent, log shared.Logger
 			group.TaskGroupID = taskID
 		}
 
-		log.Debugf("Adding task: %s, id: %s, status: %s", run.GetName(), taskID, run.GetStatus())
+		log.Debugf("Adding task: %s, id: %s, conclusion: %s", run.GetName(), taskID, run.GetConclusion())
+
+		// Match the GitHub Check Runs API to the old Taskcluster status API.
+		state := "completed"
+		if run.GetConclusion() != "success" {
+			state = run.GetConclusion()
+		}
 
 		group.Tasks = append(group.Tasks, TaskInfo{
 			Name:   run.GetName(),
 			TaskID: taskID,
-			State:  run.GetStatus(),
+			State:  state,
 		})
 	}
 
@@ -501,7 +507,7 @@ func ExtractArtifactURLs(rootURL string, log shared.Logger, group *TaskGroupInfo
 		}
 
 		if task.State != "completed" {
-			log.Infof("Task group %s has an unfinished task: %s; %s will be ignored in this group.",
+			log.Infof("Task group %s has a non-successful task: %s; %s will be ignored in this group.",
 				group.TaskGroupID, id, product)
 			failures.Add(product)
 			continue
