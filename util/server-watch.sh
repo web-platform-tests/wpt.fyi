28a65d6435d9f98fc6532280576628c505671298
Robert Ma <robertma@chromium.org>
2020-12-15 18:37:17 -0500

    Exclude webapp/components from inotifywait

--- util/server-watch.sh
+++ util/server-watch.sh
@@ -3,8 +3,9 @@ set -e
 
 ./web &
 SERVER_PID=$!
-# node_modules is already served live by packr.
-while inotifywait -q -e modify -r webapp @webapp/node_modules; do
+# node_modules is already served live by packr;
+# components is served by nicehttp from disk.
+while inotifywait -q -e modify -r webapp @webapp/node_modules @webapp/components; do
   kill $SERVER_PID
   make go_build_dev
   ./web &
