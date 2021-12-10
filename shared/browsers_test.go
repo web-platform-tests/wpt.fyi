a164555e4e8fd9ecf1ba4e9b1b7052bf6aa91c90
Weizhong Xia <weizhong@google.com>
2021-12-10 18:35:19 +0000

    Add "android_webview" and "chrome_android" as browser name

--- shared/browsers_test.go
+++ shared/browsers_test.go
@@ -18,6 +18,7 @@ func TestGetDefaultBrowserNames(t *testing.T) {
 	assert.True(t, sort.StringsAreSorted(names))
 	// Non default browser names:
 	for _, n := range names {
+		assert.NotEqual(t, "chrome_android", n)
 		assert.NotEqual(t, "android_webview", n)
 		assert.NotEqual(t, "deno", n)
 		assert.NotEqual(t, "epiphany", n)
@@ -35,6 +36,7 @@ func TestIsBrowserName(t *testing.T) {
 	assert.True(t, IsBrowserName("firefox"))
 	assert.True(t, IsBrowserName("flow"))
 	assert.True(t, IsBrowserName("safari"))
+	assert.True(t, IsBrowserName("chrome_android"))
 	assert.True(t, IsBrowserName("android_webview"))
 	assert.True(t, IsBrowserName("epiphany"))
 	assert.True(t, IsBrowserName("servo"))
--- shared/product_spec.go
+++ shared/product_spec.go
@@ -72,6 +72,10 @@ func (p ProductSpec) DisplayName() string {
 	switch p.BrowserName {
 	case "chrome":
 		return "Chrome"
+	case "chrome_android":
+		return "ChromeAndroid"
+	case "android_webview":
+		return "WebView"
 	case "deno":
 		return "Deno"
 	case "edge":
