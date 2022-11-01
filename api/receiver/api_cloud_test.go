58f99cdd3226ad2528c3db1dd59b6ac24074ad23
James Scott <jamescscott@google.com>
2022-11-01 14:20:34 -0400

    Move TestAuthenticateUploader to new cloud test
    Moved because it requires access to the cloud for secret manager

---
+++ api/receiver/api_cloud_test.go
@@ -0,0 +1,47 @@
+//go:build cloud
+// +build cloud
+
+// Copyright 2018 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package receiver
+
+import (
+	"bytes"
+	"context"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/web-platform-tests/wpt.fyi/shared"
+)
+
+// TestAuthenticateUploader relies on the setup of
+// TestCloudSecretManagerGetSecret in
+// shared/secret_manager_cloud_cloud_test.go
+func TestAuthenticateUploader(t *testing.T) {
+	// ctx, done, err := sharedtest.NewAEContext(true)
+	// assert.Nil(t, err)
+	// defer done()
+	ctx := context.Background()
+	err := shared.Clients.Init(ctx)
+	require.NoError(t, err)
+	a := NewAPI(ctx)
+
+	req := httptest.NewRequest("", "/api/foo", &bytes.Buffer{})
+	assert.Equal(t, "", AuthenticateUploader(a, req))
+
+	// Case 1: Try to get an uploader that does not exist
+	req.SetBasicAuth("bad-test-secret", "bad-value")
+	assert.Equal(t, "", AuthenticateUploader(a, req))
+
+	// Case 2: Try with correct username and password
+	req.SetBasicAuth("test-secret", "test-secret-value")
+	assert.Equal(t, "test-secret", AuthenticateUploader(a, req))
+
+	// Case 3: Try with correct username but bad password
+	req.SetBasicAuth("test-secret", "456")
+	assert.Equal(t, "", AuthenticateUploader(a, req))
+}
