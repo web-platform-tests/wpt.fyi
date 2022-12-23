5115a0d5f2cdfd20cb2ac758ce88a6a066b6602a
Weizhong Xia <weizhong@google.com>
2022-12-23 00:18:30 +0000

    fix lint

--- webapp/components/product-info.js
+++ webapp/components/product-info.js
@@ -43,7 +43,7 @@ const versionPatterns = Object.freeze({
 
 // The set of all browsers known to the wpt.fyi UI.
 const AllBrowserNames = Object.freeze(['android_webview', 'chrome_android', 'chrome_ios', 'chrome',
-    'deno', 'edge', 'firefox', 'flow', 'safari', 'servo', 'webkitgtk']);
+  'deno', 'edge', 'firefox', 'flow', 'safari', 'servo', 'webkitgtk']);
 
 // The list of default browsers used in cases where the user has not otherwise
 // chosen a set of browsers (e.g. which browsers to show runs for). Stored as
