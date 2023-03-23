d3e48241ed2ff74c15b972630d57ca0b112bf39a
Leonard Yam <yamthesmall@gmail.com>
2023-03-24 03:41:17 +0800

    Fix linting and formatting issues

--- webapp/components/interop.js
+++ webapp/components/interop.js
@@ -1,6 +1,12 @@
-import { InteropDashboard } from "./interop-dashboard.js";
-import { InteropFeatureChart } from "./interop-feature-chart.js";
-import { InteropSummary } from "./interop-summary.js";
+/**
+ * Copyright 2023 The WPT Dashboard Project. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+import { InteropDashboard } from './interop-dashboard.js';
+import { InteropFeatureChart } from './interop-feature-chart.js';
+import { InteropSummary } from './interop-summary.js';
 
 window.customElements.define(InteropFeatureChart.is, InteropFeatureChart);
 window.customElements.define(InteropSummary.is, InteropSummary);
