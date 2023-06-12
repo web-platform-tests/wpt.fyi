33959c261bed5b1e183fe5eff87b1e0cfe4cdd8e
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2023-06-12 00:14:07 -0700

    Read Interop scores of previous years from static files (#3293)
    * Read previous years interop score from static file
    
    * Add comments about results generation
    
    * changes suggested by @foolip
    
    * new numbers using tests lists from correct date
    
    * end interop scoring on Dec 31st

--- webapp/components/interop-data.js
+++ webapp/components/interop-data.js
@@ -16,7 +16,15 @@ export const interopData = {
         'score_as_group': false
       }
     ],
-    'csv_url': 'https://raw.githubusercontent.com/web-platform-tests/results-analysis/gh-pages/data/interop-2021/interop-2021-{stable|experimental}-v2.csv',
+    /**
+     * Interop scores are "frozen" after the end of the year.
+     * Once an interop year is completed, results are generated one more time
+     * from the results-analysis script for the full year, and those scores
+     * are placed for reference in the webapp/static directory. The score
+     * is no longer updated and is referenced from this location.
+     * More information at https://github.com/web-platform-tests/results-analysis
+    **/
+    'csv_url': '/static/interop-2021-{stable|experimental}.csv',
     'summary_feature_name': 'summary',
     'matrix_url': 'https://matrix.to/#/#interop20xx:matrix.org?web-instance%5Belement.io%5D=app.element.io',
     'focus_areas': {
@@ -117,8 +125,16 @@ export const interopData = {
         ]
       }
     ],
-    'investigation_weight': 0.0,
-    'csv_url': 'https://raw.githubusercontent.com/web-platform-tests/results-analysis/gh-pages/data/interop-2022/interop-2022-{stable|experimental}-v2.csv',
+    'investigation_weight': 0.1,
+    /**
+     * Interop scores are "frozen" after the end of the year.
+     * Once an interop year is completed, results are generated one more time
+     * from the results-analysis script for the full year, and those scores
+     * are placed for reference in the webapp/static directory. The score
+     * is no longer updated and is referenced from this location.
+     * More information at https://github.com/web-platform-tests/results-analysis
+    **/
+    'csv_url': '/static/interop-2022-{stable|experimental}.csv',
     'summary_feature_name': 'summary',
     'issue_url': 'https://github.com/web-platform-tests/interop/issues/new',
     'focus_areas': {
@@ -311,6 +327,10 @@ export const interopData = {
       }
     ],
     'investigation_weight': 0.0,
+    /**
+     * More information on results generation at
+     * https://github.com/web-platform-tests/results-analysis
+    **/
     'csv_url': 'https://raw.githubusercontent.com/web-platform-tests/results-analysis/gh-pages/data/interop-2023/interop-2023-{stable|experimental}-v2.csv',
     'summary_feature_name': 'summary',
     'issue_url': 'https://github.com/web-platform-tests/interop/issues/new',
