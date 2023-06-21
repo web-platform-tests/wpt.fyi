3e687250c198db9adc02407f3e8077e3eca42062
Rachel <racheljay42@gmail.com>
2023-06-20 21:20:42 -0700

    square row tiles in correct color

--- webapp/components/new-test-results-history-grid.js
+++ webapp/components/new-test-results-history-grid.js
@@ -1,11 +1,9 @@
 import { PolymerElement, html } from '../node_modules/@polymer/polymer/polymer-element.js';
-import { TestFileResults } from './test-file-results.js';
 import { LoadingState } from './loading-state.js';
-// import { WPTColors } from '../components/wpt-colors.js';
 
 class TestResultsGrid extends LoadingState(PolymerElement) {
-	static get template() {
-		return html`
+  static get template() {
+    return html`
         <style>
             .browser {
                 display: flex;
@@ -24,54 +22,60 @@ class TestResultsGrid extends LoadingState(PolymerElement) {
             .square.FAIL {
                 background-color: var(--paper-red-300)
             }
+						.subtest-row {
+							display: flex;
+						}
         </style>
         <template is="dom-repeat" items="[[dataKeys]]" as="dataKey" index-as="i">
-
-       <div
-        class="browser"
-        >[[data.dataKey]]</div>
-				<p>[[dataKey]]</p>
-        <div class$="square [[data.summary.0.status]]"></div> 
+				<h2>[[dataKey]]</h2>
+				<div class="subtest-row">
+						<template is="dom-repeat" items="[[getSubTests(dataKey)]]" as="subTest">
+							<div class$="square [[subTest.status]]"></div>
+							</template>
+							</div>
         </template>
-        `
-	}
+        `;
+  }
 
-	constructor() {
-		super();
-		this.getTestHistory()
-	}
+  constructor() {
+    super();
+    this.getTestHistory();
+  }
 
-	static get is() {
-		return 'new-test-results-history-grid';
-	}
+  static get is() {
+    return 'new-test-results-history-grid';
+  }
 
-	static get properties() {
-		return {
-			data: {
-				type: Object,
-				value: {},
-			},
-			dataKeys: {
-				type: Array,
-				value: [],
-			}
-		}
-	}
+  static get properties() {
+    return {
+      data: {
+        type: Object,
+        value: {},
+      },
+      dataKeys: {
+        type: Array,
+        value: [],
+      }
+    };
+  }
 
-	async getTestHistory() {
-		const url = new URL('/api/history', window.location)
+  async getTestHistory() {
+    const url = new URL('/api/history', window.location);
 
-		this.data = await this.load(
-			window.fetch(url).then(r => r.json()).then(data => {
-				return data
-			})
-		);
-		this.dataKeys = Object.keys(this.data)
-		console.log("data keys", this.dataKeys)
-	}
+    this.data = await this.load(
+      window.fetch(url).then(r => r.json()).then(data => {
+        return data;
+      })
+    );
+    this.dataKeys = Object.keys(this.data);
+  }
+
+  getSubTests(key) {
+    return this.data[key];
+  }
 }
 
 
-window.customElements.define(TestResultsGrid.is, TestResultsGrid)
+window.customElements.define(TestResultsGrid.is, TestResultsGrid);
 
-export { TestResultsGrid }
+export { TestResultsGrid };
