1f580f067f4c04af527d17e82319ee3c86c19d8a
Rachel <racheljay42@gmail.com>
2023-06-28 17:53:04 -0700

    integrate google charts

--- webapp/components/new-test-results-history-grid.js
+++ webapp/components/new-test-results-history-grid.js
@@ -1,100 +1,185 @@
 import { PolymerElement, html } from '../node_modules/@polymer/polymer/polymer-element.js';
-import { LoadingState } from './loading-state.js';
 
-class TestResultsGrid extends LoadingState(PolymerElement) {
+const PASS_COLOR = '#81c784';
+const FAIL_COLOR = '#e57373';
+const NEUTRAL_COLOR = '#eeeeee';
+const COLOR_MAPPING = {
+  // Passing statuses
+  OK: PASS_COLOR,
+  PASS: PASS_COLOR,
+
+  // Failing statuses
+  CRASHED: FAIL_COLOR,
+  ERROR: FAIL_COLOR,
+  FAIL: FAIL_COLOR,
+  NOTRUN: FAIL_COLOR,
+  PRECONDITION_FAILED: FAIL_COLOR,
+  TIMEOUT: FAIL_COLOR,
+
+  // Neutral statuses
+  MISSING: NEUTRAL_COLOR,
+  SKIPPED: NEUTRAL_COLOR,
+  default: NEUTRAL_COLOR,
+};
+
+class TestResultsGrid extends PolymerElement {
   static get template() {
     return html`
         <style>
-            .browser {
-                display: flex;
-                border: 2px dotted orange;
-            }
-            .grid-container {
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-              width: 20rem;
-            }
-            .square {
-							 	border: 1px solid gray;
-                border-radius: 0.2rem;
-                height: 1rem;
-                margin: 1px;
-                width: 1rem;
-            }
-            .square.OK, .square.PASS {
-                background-color: var(--paper-green-300);
-            }
-            .square.FAIL, .square.TIMEOUT {
-                background-color: var(--paper-red-300)
-            }
-						.subtest-row {
-							display: flex;
-              justify-content: right;
-						}
-            .test-name {
-              padding-right: 2rem;
-            }
+          .chart {
+            height: 15rem;
+          }
+          .chart rect, .chart text {
+            cursor: pointer;
+          }
         </style>
-        <div class="grid-container">
-          <template is="dom-repeat" items="[[subtestNames]]" as="subtestName">
-            <div class="subtest-row">
-              <span class="test-name">[[subtestName]]</span>
-              <template is="dom-repeat" items="[[runs]]" as="run">
-              <a href="[[getRunLink(run)]]">
-                <div class$="[[getSquareClass(subtestName, run)]]"></div>
-              </a>
-              </template>
-            </div>
-          </template>
-        </div>
+
+        <div class="chart" id="chromeHistoryChart"></div>
+        <div class="chart" id="edgeHistoryChart"></div>
+        <div class="chart" id="firefoxHistoryChart"></div>
+        <div class="chart" id="safariHistoryChart"></div>
         `;
   }
 
-  constructor() {
-    super();
-    this.getTestHistory();
+  static get properties() {
+    return {
+      dataTable: Object,
+      runIDs: Array,
+    };
   }
 
   static get is() {
     return 'new-test-results-history-grid';
   }
 
-  // convert run id into url
-  getRunLink(run) {
-    return `/results/?run_id=${run.id}`;
+  constructor() {
+    super();
+
+    // Get the test history data and then populate the chart
+    this.getTestHistory();
+    this.loadCharts().then(() => this.updateAllCharts(this.historicalData));
+
+    // Google Charts is not responsive, even if one sets a percentage-width, so
+    // we add a resize observer to redraw the chart if the size changes.
+    window.addEventListener('resize', () => {
+      this.updateAllCharts(this.historicalData);
+    });
   }
 
-  // return color for square class based on
-  // timeline of historical tests
-  getSquareClass(subtestName, run) {
-    const runDate = new Date(run.time_start);
-    const historyResults = this.historicalData[subtestName];
+  // Load Google charts for test history display
+  async loadCharts() {
+    await window.google.charts.load('current', { packages: ['timeline'] });
+  }
 
-    let colorClass;
-    for (let i = historyResults.length - 1; i >= 0; i--) {
-      const historicalDate = new Date(Number(historyResults[i].date));
+  updateAllCharts(historicalData) {
+    const divNames = [
+      'chromeHistoryChart',
+      'edgeHistoryChart',
+      'firefoxHistoryChart',
+      'safariHistoryChart'
+    ];
 
-      if (runDate > historicalDate || i === 0) {
-        colorClass = historyResults[i].status;
-        break;
-      }
+    divNames.forEach((name, i) => {
+      this.updateChart(historicalData[i], name);
+    });
+  }
+
+  updateChart(browserTestData, divID) {
+    // Our observer may be called before the historical data has been fetched,
+    // so debounce that.
+    if (!browserTestData) {
+      return;
     }
-    return `square ${colorClass}`;
+
+    // Fetching the data table first ensures that Google Charts has been loaded.
+    // Using timeline chart
+    // https://developers.google.com/chart/interactive/docs/gallery/timeline
+    const div = this.$[divID];
+    this.chart = new window.google.visualization.Timeline(div);
+
+    this.dataTable = new window.google.visualization.DataTable();
+
+    // Set up columns, including tooltip information and style guidelines
+    this.dataTable.addColumn({ type: 'string', id: 'Subtest' });
+    this.dataTable.addColumn({ type: 'string', id: 'Status' });
+
+    // style and tooltip columns that are not displayed
+    this.dataTable.addColumn({ type: 'string', id: 'style', role: 'style' });
+    this.dataTable.addColumn({ type: 'string', role: 'tooltip' });
+
+    this.dataTable.addColumn({ type: 'date', id: 'Start' });
+    this.dataTable.addColumn({ type: 'date', id: 'End' });
+
+    const dataTableRows = [];
+    const now = new Date();
+    this.runIDs = [];
+
+    // Create a row for each subtest
+    this.subtestNames.forEach(subtestName => {
+      for (let i = 0; i < browserTestData[subtestName].length; i++) {
+        const dataPoint = browserTestData[subtestName][i];
+        const startDate = new Date(Number(dataPoint.date));
+
+        // Use the next entry as the end date, or use present time if this
+        // is the last entry
+        let endDate = now;
+        if (i + 1 !== browserTestData[subtestName].length) {
+          const nextDataPoint = browserTestData[subtestName][i + 1];
+          endDate = new Date(Number(nextDataPoint.date));
+        }
+
+        // If this is the main test status, name it based on the amount of subtests
+        let subtestDisplayName = subtestName;
+        if (subtestName === '') {
+          subtestDisplayName = (this.subtestNames.length > 1) ? 'Harness status' : 'Test status';
+        }
+
+        const tooltip =
+          `${dataPoint.status} ${startDate.toLocaleDateString()}-${endDate.toLocaleDateString()}`;
+        const statusColor = COLOR_MAPPING[dataPoint.status] || COLOR_MAPPING.default;
+
+        // Add the run ID to array of run IDs to use for links
+        this.runIDs.push(dataPoint.run_id);
+
+        dataTableRows.push([
+          subtestDisplayName,
+          dataPoint.status,
+          statusColor,
+          tooltip,
+          startDate,
+          endDate,
+        ]);
+      }
+    });
+
+    let options = {
+      tooltip: {
+        isHtml: false,
+      },
+    };
+    this.dataTable.addRows(dataTableRows);
+
+    // handler to allow rows to be clicked and navigate to the run url
+    // https://stackoverflow.com/questions/40928971/how-to-customize-google-chart-with-hyperlink-in-the-label
+    const statusSelectHandler = () => {
+      const selection = this.chart.getSelection();
+      if (selection.length > 0) {
+        const index = selection[0].row;
+        if (index !== undefined && this.runIDs.length > index) {
+          window.open(`/results/?run_id=${this.runIDs[index]}`, '_blank');
+        }
+      }
+    };
+    window.google.visualization.events.addListener(this.chart, 'select', statusSelectHandler);
+
+    this.chart.draw(this.dataTable, options);
   }
 
   // get test history and aligned run data
   async getTestHistory() {
-    this.historicalData = await fetch('/api/history').then(r => r.json()).then(data => data);
-    this.subtestNames = Object.keys(this.historicalData);
-
-    this.runs = await fetch('/api/runs?label=master&label=experimental&max-count=100&aligned')
-      .then(r => r.json());
-    this.runs = this.runs.filter(run => run.browser_name === 'chrome');
-  }
-
-  getSubTests(key) {
-    return this.data[key];
+    this.historicalData = await fetch('/api/history')
+      .then(r => r.json()).then(data => data.results);
+    this.subtestNames = Object.keys(this.historicalData[0]);
   }
 }
 
