7243b9d0c4b05c4b09022b9f599201398b436440
Rachel <racheljay42@gmail.com>
2023-08-28 13:52:40 -0700

    fixed broken test

--- api/test_history_test.go
+++ api/test_history_test.go
@@ -34,36 +34,37 @@ func TestTestHistoryHandler(t *testing.T) {
 
 	store := shared.NewAppEngineDatastore(ctx, false)
 	key := store.NewIncompleteKey("TestHistoryEntry")
-	_, err = store.Put(key, []shared.TestHistoryEntry{sampleRun})
+	_, err = store.Put(key, &sampleRun)
 	assert.Nil(t, err)
 
 	bodyReader := strings.NewReader(body)
 	r := httptest.NewRequest("POST", "/api/history", bodyReader)
 	w := httptest.NewRecorder()
 	testHistoryHandler(w, r)
-	// results := parseHistoryResponse(t, w)
+	results := parseHistoryResponse(t, w)
 
-	// want := map[string]map[string]map[string][]map[string]string{
-	// "results": {
-	// 	"chrome": {
-	// 		"": {
-	// 			{
-	// 				"date":   "2022-06-02T06:02:55.000Z",
-	// 				"status": "TIMEOUT",
-	// 				"run_id": "5074677897101312",
-	// 			},
-	// 		},
-	// 	},
-	// },
-	// }
-	assert.Equal(t, true, true)
+	want := map[string]map[string]Browser{
+		"results": {
+			"chrome": {
+				"": {
+					{
+						"date":   "2022-06-02T06:02:55.000Z",
+						"status": "PASS",
+						"run_id": "123",
+					},
+				},
+			},
+		},
+	}
+
+	assert.Equal(t, want, results)
 
 }
 
-func parseHistoryResponse(t *testing.T, w *httptest.ResponseRecorder) []string {
+func parseHistoryResponse(t *testing.T, w *httptest.ResponseRecorder) map[string]map[string]Browser {
 	assert.Equal(t, http.StatusOK, w.Result().StatusCode)
 	out, _ := io.ReadAll(w.Body)
-	var result []string
+	var result map[string]map[string]Browser
 	json.Unmarshal(out, &result)
 	return result
 }
