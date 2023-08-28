15017fade0b485843adfdaadf96952ee0d0cce80
Rachel <racheljay42@gmail.com>
2023-08-28 14:22:49 -0700

    copyright blurb

--- api/test_history_test.go
+++ api/test_history_test.go
@@ -1,3 +1,10 @@
+//go:build medium
+// +build medium
+
+// Copyright 2023 The WPT Dashboard Project. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package api
 
 import (
