4bb601432f40deddcadafc55bfb3a75ec2f53734
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2023-09-19 07:40:15 -0700

    Update new-test-results-history-grid.js

--- webapp/components/new-test-results-history-grid.js
+++ webapp/components/new-test-results-history-grid.js
@@ -244,7 +244,11 @@ class TestResultsGrid extends PathInfo(PolymerElement) {
     window.google.visualization.events.addListener(
       this.charts[chartIndex], 'select', () => statusSelectHandler(chartIndex));
 
-    this.charts[chartIndex].draw(this.dataTable, options);
+    if (dataTableRows > 0) {
+      this.charts[chartIndex].draw(this.dataTable, options);
+    } else {
+      div.innerHTML = 'No browser historical data found for this test.';
+    }
   }
 
   // get test history and aligned run data
