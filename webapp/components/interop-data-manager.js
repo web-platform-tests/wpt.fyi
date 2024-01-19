4ee8d7278a868f59721f169efefcd30881ee1e7d
DanielRyanSmith <danielrsmith@google.com>
2024-01-19 05:29:36 +0000

    add back investigations

--- webapp/components/interop-data-manager.js
+++ webapp/components/interop-data-manager.js
@@ -200,6 +200,7 @@ class InteropDataManager {
             throw new Error(`Expected score in 0-1000 range, got ${score}`);
           }
           const tooltip = this.createTooltip(browserName, version, score);
+          console.log(feature);
           newRows.get(feature).push(score / 1000);
           newRows.get(feature).push(tooltip);
 
