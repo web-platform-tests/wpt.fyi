0d92e29f6bfb1c3f1c36770e9fd53240cf88f8bc
DanielRyanSmith <danielrsmith@google.com>
2024-01-20 03:54:08 +0000

    lint

--- webapp/components/interop-data.js
+++ webapp/components/interop-data.js
@@ -669,7 +669,7 @@ export const interopData = {
      * More information on results generation at
      * https://github.com/web-platform-tests/results-analysis
     **/
-    "csv_url": "https://raw.githubusercontent.com/web-platform-tests/results-analysis/gh-pages/data/interop-2024/interop-2024-{stable|experimental}-v2.csv",
+    "csv_url": 'https://raw.githubusercontent.com/web-platform-tests/results-analysis/gh-pages/data/interop-2024/interop-2024-{stable|experimental}-v2.csv',
     'summary_feature_name': 'summary',
     'issue_url': 'https://github.com/web-platform-tests/interop/issues/new',
     'focus_areas': {
