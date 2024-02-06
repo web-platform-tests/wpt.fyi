05ddddc52a6b95469131eac5e439af39cbd1200a
Daniel Smith <56164590+DanielRyanSmith@users.noreply.github.com>
2024-02-06 09:08:45 -0800

    Update Interop color logic for summary scores (#3668)
    * update interop color calculation logic
    
    * Update interop-summary.js
    
    * separate text and background color
    
    * Revert "separate text and background color"
    
    This reverts commit 76c0c72766bbf4d7fcead9bdc2daad89ae7eb75f.
    
    * changes suggested by @foolip and @zcorpan
    
    ---------
    
    Co-authored-by: DanielRyanSmith <danielrsmith@google.com>

--- webapp/components/interop-summary.js
+++ webapp/components/interop-summary.js
@@ -221,7 +221,7 @@ class InteropSummary extends PolymerElement {
       startVal: curScore === '--' ? 0 : curScore
     }).start();
     const colors = this.calculateColor(score);
-    number.style.color = colors[0];
+    number.style.color = `color-mix(in lch, ${colors[0]} 70%, black)`;
     number.style.backgroundColor = colors[1];
   }
 
@@ -285,19 +285,36 @@ class InteropSummary extends PolymerElement {
   }
 
   calculateColor(score) {
-    if (score >= 95) {
-      return ['#388E3C', '#00c70a1a'];
+    const gradient = [
+      // Red.
+      { scale: 0, color: [250, 0, 0] },
+      // Orange.
+      { scale: 33.33, color: [250, 125, 0] },
+      // Yellow.
+      { scale: 66.67, color: [220, 220, 0] },
+      // Green.
+      { scale: 100, color: [0, 160, 0] },
+    ];
+
+    let color1, color2;
+    for (let i = 1; i < gradient.length; i++) {
+      if (score <= gradient[i].scale) {
+        color1 = gradient[i - 1];
+        color2 = gradient[i];
+        break;
+      }
     }
-    if (score >= 75) {
-      return ['#568f24', '#64d60026'];
-    }
-    if (score >= 50) {
-      return ['#b88400', '#ffc22926'];
-    }
-    if (score >= 25) {
-      return ['#d16900', '#f57a0026'];
-    }
-    return ['#ee2b2b', '#ff050526'];
+    const colorWeight = ((score - color1.scale) / (color2.scale - color1.scale));
+    const color = [
+      Math.round(color1.color[0] * (1 - colorWeight) + color2.color[0] * colorWeight),
+      Math.round(color1.color[1] * (1 - colorWeight) + color2.color[1] * colorWeight),
+      Math.round(color1.color[2] * (1 - colorWeight) + color2.color[2] * colorWeight),
+    ];
+
+    return [
+      `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+      `rgba(${color[0]}, ${color[1]}, ${color[2]}, 0.15)`,
+    ];
   }
 }
 export { InteropSummary };
