032467be03bfa14b5bc9a0c0d594730b4ab8874f
sj0602 <sj0602@users.noreply.github.com>
2024-03-20 15:39:08 -0700

    Support chrome ios wptrunner result in FYI (#3754)
    * Support chrome ios wptrunner result in FYI
    * Update chrome ios wptrunner logo

--- webapp/components/product-info.js
+++ webapp/components/product-info.js
@@ -11,6 +11,7 @@ const DisplayNames = (() => {
   ['safari', 'safari-experimental'].forEach(n => m.set(n, 'Safari'));
   m.set('android_webview', 'WebView');
   m.set('chrome_android', 'ChromeAndroid');
+  m.set('chrome_ios', 'ChromeIOS');
   m.set('chromium', 'Chromium');
   m.set('deno', 'Deno');
   m.set('firefox_android', 'Firefox Android');
@@ -46,7 +47,7 @@ const versionPatterns = Object.freeze({
 });
 
 // The set of all browsers known to the wpt.fyi UI.
-const AllBrowserNames = Object.freeze(['android_webview', 'chrome_android', 'chrome',
+const AllBrowserNames = Object.freeze(['android_webview', 'chrome_android', 'chrome_ios', 'chrome',
   'chromium', 'deno', 'edge', 'firefox_android', 'firefox', 'flow', 'node.js', 'safari', 'servo', 'webkitgtk', 'wktr']);
 
 // The list of default browsers used in cases where the user has not otherwise
@@ -172,9 +173,9 @@ const ProductInfo = (superClass) => class extends superClass {
     } else if (name === 'android_webview') {
       return `/static/${name}.png`;
 
-    } else if (name === 'chrome_android') {
+    } else if (name === 'chrome_android' || name === 'chrome_ios') {
       // TODO(kyle): A temporary workaround; remove this check when
-      // chrome_android is mapped to chrome on wptrunner.
+      // chrome_android and chrome_ios is mapped to chrome on wptrunner.
       return '/static/chrome_64x64.png';
     } else if (name === 'firefox_android') {
       // For now use the geckoview logo for Firefox for Android,
