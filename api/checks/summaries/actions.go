c1af4ad149a5e6c6e19edeb7ff8190ba26666f7b
Panagiotis Astithas <pastith@gmail.com>
2024-09-20 17:10:08 -0700

    Upgrade go-github to v65

--- api/checks/summaries/actions.go
+++ api/checks/summaries/actions.go
@@ -4,7 +4,7 @@
 
 package summaries
 
-import "github.com/google/go-github/v47/github"
+import "github.com/google/go-github/v65/github"
 
 // RecomputeAction is an action that can be taken to
 // trigger a recompute of the diff, against the latest
