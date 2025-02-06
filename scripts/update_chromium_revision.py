67557473fb095cbbcab967316e4f4e224a94bf09
DanielRyanSmith <danielrsmith@google.com>
2025-02-06 22:40:25 +0000

    make similar changes to update_chromium_revision

--- scripts/update_chromium_revision.py
+++ scripts/update_chromium_revision.py
@@ -7,48 +7,53 @@ import os
 import requests
 from datetime import date
 from google.cloud import storage
-from google.cloud import secretmanager
 
 
 BUCKET_NAME = 'wpt-versions'
 NEW_REVISION_FILE = 'pinned_chromium_revision_NEW'
 OLD_REVISION_FILE = 'pinned_chromium_revision'
-REPO_OWNER = "web-platform-tests"
-REPO_NAME = "wpt"
-PR_NUMBER = "50375"
 
 
-def all_passing_checks() -> bool:
+def all_passing_checks(repo_owner: str, repo_name: str, pr_number: str) -> bool:
     """Check if all CI tests passed."""
     s = requests.Session()
+    sha = get_sha(repo_owner, repo_name, pr_number)
     s.headers.update({
-        "Authorization": f"token {get_token()}"
+        'Authorization': f'token {get_token()}',
+        # Specified API version. See https://docs.github.com/en/rest/about-the-rest-api/api-versions
+        'X-GitHub-Api-Version': '2022-11-28',
     })
-    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/commits/{get_sha()}/check-suites"
+    url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/commits/{sha}/check-suites'
     response = s.get(url)
     if response.status_code != 200:
-        print(f"Received response status {response.status_code} from {url}")
+        print(f'Received response status {response.status_code} from {url}')
     check_info = response.json()
-    for check in check_info["check_suites"]:
-        if check["conclusion"] != "success":
+    for check in check_info['check_suites']:
+        if check['conclusion'] != 'success':
             return False
     return True
 
 
-def update_pr_body(new_revision: str, tests_passed: bool) -> bool:
-    outcome = "Passed" if tests_passed else "Failed"
+def update_pr_body(
+    new_revision: str,
+    tests_passed: bool,
+    repo_owner: str,
+    repo_name: str,
+    pr_number: str,
+) -> bool:
+    outcome = 'Passed' if tests_passed else 'Failed'
     body = (
-        "This pull request is used for automated runs of the WPT check suites "
-        "against a new available Chromium revision. If all tests pass, the new "
-        "revision will be pinned for use.\\n\\nLast revision checked: "
-        f"{new_revision.decode('utf-8')}\\nCheck run date: {date.today()}"
-        f"\\nOutcome: **{outcome}**"
+        'This pull request is used for automated runs of the WPT check suites '
+        'against a new available Chromium revision. If all tests pass, the new '
+        'revision will be pinned for use.\\n\\nLast revision checked: '
+        f'{new_revision.decode('utf-8')}\\nCheck run date: {date.today()}'
+        f'\\nOutcome: **{outcome}**'
     )
 
     body = '{"body":"' + body + '"}'
     s = requests.Session()
-    s.headers.update({"Authorization": f"token {get_token()}"})
-    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/pulls/{PR_NUMBER}"
+    s.headers.update({'Authorization': f'token {get_token()}'})
+    url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/pulls/{pr_number}'
     response = s.patch(url, data=body)
     return response.status_code == 200
 
@@ -71,27 +76,32 @@ def update_chromium_revision(new_revision) -> None:
 
 def get_token() -> str:
     """Get token to check on the CI runs."""
-    return os.environ["GIT_CHECK_PR_STATUS_TOKEN"]
+    return os.environ['GIT_CHECK_PR_STATUS_TOKEN']
 
 
-def get_sha() -> str:
+def get_sha(repo_owner: str, repo_name: str, pr_number: str) -> str:
     """Get head sha from PR."""
-    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/pulls/{PR_NUMBER}"
+    url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/pulls/{pr_number}'
     s = requests.Session()
     response = s.get(url)
     pr_info = response.json()
-    return pr_info["head"]["sha"]
+    return pr_info['head']['sha']
 
 
 def main(args, _):
-    tests_passed = all_passing_checks()
+    repo_owner = os.environ['REPO_OWNER']
+    repo_name = os.environ['REPO_NAME']
+    pr_number = os.environ['PR_NUMBER']
+
+    tests_passed = all_passing_checks(repo_owner, repo_name, pr_number)
     new_revision = get_new_revision()
+
     if tests_passed:
         update_chromium_revision(new_revision)
-    if not update_pr_body(new_revision, tests_passed):
-        print("Failed to update PR body description.")
+    if not update_pr_body(new_revision, tests_passed, repo_owner, repo_name, pr_number):
+        print('Failed to update PR body description.')
     if tests_passed:
-        print(f"Revision updated to {new_revision}.")
-        return f"Revision updated to {new_revision}."
-    print(f"Some checks failed for PR {PR_NUMBER}. Revision not updated.")
-    return f"Some checks failed for PR {PR_NUMBER}. Revision not updated."
+        print(f'Revision updated to {new_revision}.')
+        return f'Revision updated to {new_revision}.'
+    print(f'Some checks failed for PR {pr_number}. Revision not updated.')
+    return f'Some checks failed for PR {pr_number}. Revision not updated.'
